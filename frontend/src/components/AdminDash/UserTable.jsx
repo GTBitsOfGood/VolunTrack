0b171dce12b2639d60e63f7dcc0f79f37841b09c
Daniel Hsing <hsing.daniel@gmail.com>
2019-09-21 16:29:22 -0400

    chore: remove drawchange volunteer UI and related styles
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- frontend/src/components/AdminDash/UserTable.jsx
+++ frontend/src/components/AdminDash/UserTable.jsx
@@ -195,7 +195,6 @@ class UserTable extends React.Component {
             </ModalFooter>
           </Form>
         </Modal>
-        );
       </Styled.Container>
     );
   }
--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,14 +1,1 @@
-import React, { Component } from 'react';
-import Footer from './Footer';
-
-class Splash extends Component {
-  render() {
-    return (
-      <div>
-        <Footer onAuth={this.props.onAuth} />
-      </div>
-    );
-  }
-}
-
-export default Splash;
+export { default } from './Footer';
