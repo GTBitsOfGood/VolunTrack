0f4ac5b4d2fc5693290bdcfe164cc31389d4c10e
Daniel Hsing <hsing.daniel@gmail.com>
2019-09-21 16:15:47 -0400

    chore: remove unused image galleries, remove DrawChange splash page
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
-import styles from '../../styles/Home.module.css';
-import Gallery from 'react-grid-gallery';
-import Intro from './Intro';
 import Footer from './Footer';
-import { IMAGES } from '../../images/gallery';
 
 class Splash extends Component {
   render() {
     return (
       <div>
-        <Intro />
-        <div className={styles.gallery}>
-          <Gallery enableImageSelection={false} maxRows={1} images={IMAGES} />
-        </div>
         <Footer onAuth={this.props.onAuth} />
       </div>
     );
--- frontend/src/components/VolunteerDash/index.js
+++ frontend/src/components/VolunteerDash/index.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import Gallery from 'react-grid-gallery';
 
 import Intro from './Intro';
 import Footer from './Footer';
 import AcceptPolicies from '../AcceptPolicies';
 
 import styles from '../../styles/VolunteerDash.module.css';
-import { IMAGES } from '../../images/gallery';
 
 export default class VolunteerDashboard extends Component {
   state = {
@@ -20,9 +18,7 @@ export default class VolunteerDashboard extends Component {
         {this.state.hasPolicies ? (
           <div>
             <Intro />
-            <div className={styles.gallery}>
-              <Gallery enableImageSelection={false} maxRows={1} images={IMAGES} />
-            </div>
+            <div className={styles.gallery}></div>
             <Footer />
           </div>
         ) : (
