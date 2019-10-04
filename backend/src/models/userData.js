7db3253bbcce1c8b882fa1354d41c7a37428cbb0
Bingyao Wang <bingyaowang.nj@gmail.com>
2019-10-03 21:16:38 -0400

    model update

--- backend/src/models/userData.js
+++ backend/src/models/userData.js
@@ -6,7 +6,7 @@ const userDataSchema = new mongoose.Schema(
     role: {
       type: String,
       default: 'new',
-      enum: ['new', 'pending', 'admin', 'volunteer', 'manager', 'deleted']
+      enum: ['admin', 'volunteer', 'manager']
     },
     status: {
       type: String,
@@ -14,8 +14,6 @@ const userDataSchema = new mongoose.Schema(
       enum: [
         'accepted',
         'denied',
-        'no_response',
-        'got_initial_email',
         'has_volunteered',
         'new'
       ]
@@ -31,93 +29,21 @@ const userDataSchema = new mongoose.Schema(
       state: { type: String },
       zip_code: { type: String }
     },
-    history: {
-      volunteer_interest_cause: { type: String },
-      volunteer_support: { type: String },
-      volunteer_commitment: { type: String },
-      previous_volunteer_experience: { type: String }
-    },
-    availability: {
-      weekday_mornings: { type: Boolean, default: false },
-      weekday_afternoons: { type: Boolean, default: false },
-      weekday_evenings: { type: Boolean, default: false },
-      weekend_mornings: { type: Boolean, default: false },
-      weekend_afternoons: { type: Boolean, default: false },
-      weekend_evenings: { type: Boolean, default: false }
-    },
-    skills_interests: {
-      admin_office: { type: Boolean, default: false },
-      admin_virtual: { type: Boolean, default: false },
-      atlanta_shelter: { type: Boolean, default: false },
-      orlando_shelter: { type: Boolean, default: false },
-      graphic_web_design: { type: Boolean, default: false },
-      special_events: { type: Boolean, default: false },
-      grant_writing: { type: Boolean, default: false },
-      writing_editing: { type: Boolean, default: false },
-      social_media: { type: Boolean, default: false },
-      fundraising: { type: Boolean, default: false },
-      finance: { type: Boolean, default: false },
-      office_maintenance_housekeeping: { type: Boolean, default: false },
-      international_projects: { type: Boolean, default: false },
-      volunteer_coordination: { type: Boolean, default: false },
-      outreach: { type: Boolean, default: false },
-      languages: { type: String, default: '' },
-      skills_qualifications: { type: String }
-    },
-    referral: {
-      friend: { type: Boolean, default: false },
-      newsletter: { type: Boolean, default: false },
-      event: { type: Boolean, default: false },
-      volunteer_match: { type: Boolean, default: false },
-      internet: { type: Boolean, default: false },
-      social_media: { type: Boolean, default: false }
+    history: { type: String },
+    skills_interests: {type: String
+      //list[] skills
     },
     employment: {
-      name: { type: String },
-      position: { type: String },
-      duration: { type: String },
-      location: { type: String },
-      previous_name: { type: String },
-      previous_reason_for_leaving: { type: String },
-      previous_location: { type: String }
+      industry: {type: String},
+        occupation: {type: [String]
     },
-    reference: {
-      name: { type: String },
-      phone_number: { type: String },
-      email: { type: String },
-      relationship: { type: String },
-      duration: { type: String }
-    },
-    criminal: {
-      felony: { type: Boolean },
-      sexual_violent: { type: Boolean },
-      drugs: { type: Boolean },
-      driving: { type: Boolean },
-      none: { type: Boolean },
-      explanation: { type: String }
-    },
-    ice: {
-      name: { type: String },
-      relationship: { type: String },
-      phone_number: { type: String },
-      email: { type: String },
-      address: { type: String }
-    },
-    permissions: {
-      comments: { type: String },
-      reference: { type: Boolean },
-      personal_image: { type: Boolean },
-      email_list: { type: Boolean },
-      signature: { type: String }
-    },
-    comments: { type: String }
+    admin_comments: { type: String }
 
     // events: {
     //   type: Array, // array of event objects
     //   default: []
     // }
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 userDataSchema.virtual('name').get(function() {
