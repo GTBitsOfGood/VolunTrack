e81fd17326f9750496a68c6d21a51608686c6325
Bingyao Wang <bingyaowang.nj@gmail.com>
2019-10-11 15:17:43 -0400

    upd

--- backend/src/models/userData.js
+++ backend/src/models/userData.js
@@ -2,43 +2,45 @@ const mongoose = require('mongoose');
 
 // define schema for user collection (user model)
 const userDataSchema = new mongoose.Schema(
-  {
-    role: {
-      type: String,
-      default: 'new',
-      enum: ['admin', 'volunteer', 'manager']
-    },
-    status: {
-      type: String,
-      default: 'new',
-      enum: [
-        'accepted',
-        'denied',
-        'has_volunteered',
-        'new'
-      ]
-    },
-    bio: {
-      first_name: { type: String },
-      last_name: { type: String },
-      phone_number: { type: String },
-      email: { type: String, index: true, unique: true },
-      date_of_birth: { type: Date },
-      street_address: { type: String },
-      city: { type: String },
-      state: { type: String },
-      zip_code: { type: String }
-    },
-    history: { type: String },
-    skills_interests: {type: String
-      //list[] skills
-    },
-    employment: {
-        industry: {type: String},
-        occupation: {
-            type: [String]
+    {
+        role: {
+            type: String,
+            default: 'new',
+            enum: ['admin', 'volunteer', 'manager']
         },
-        admin_comments: {type: String}
+        status: {
+            type: String,
+            default: 'new',
+            enum: [
+                'accepted',
+                'denied',
+                'has_volunteered',
+                'new'
+            ]
+        },
+        bio: {
+            first_name: {type: String},
+            last_name: {type: String},
+            phone_number: {type: String},
+            email: {type: String, index: true, unique: true},
+            date_of_birth: {type: Date},
+            street_address: {type: String},
+            city: {type: String},
+            state: {type: String},
+            zip_code: {type: String}
+        },
+        history: {type: String},
+        skills_interests: {
+            type: String
+            //list[] skills
+        },
+        employment: {
+            industry: {type: String},
+            occupation: {
+                type: [String]
+            },
+            admin_comments: {type: String}
+        }
     },
 
     // events: {
