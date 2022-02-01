62be48d58ac485e6e68500efe607778b8b27a4a5
jackpalaia <jack@jackpalaia.com>
2022-02-01 08:34:38 -0500

    add prettier-plugin-organize-imports

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
-import NextAuth from "next-auth";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "lib/mongodb";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
