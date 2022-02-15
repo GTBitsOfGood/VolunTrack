10b949dca9a31247f4b6a4fd98a36644fb659fd2
jackpalaia <jack@jackpalaia.com>
2022-02-15 16:56:41 -0500

    make attempt at css

--- src/screens/Login/LoginPage.jsx
+++ src/screens/Login/LoginPage.jsx
@@ -1,6 +1,7 @@
 import { signIn } from "next-auth/react";
 import React from "react";
 import GoogleButton from "react-google-button";
+import variables from "../../design-tokens/_variables.module.scss";
 
 const LoginPage = () => {
   const login = (e) => {
@@ -8,7 +9,40 @@ const LoginPage = () => {
     signIn("google");
   };
 
-  return <GoogleButton onClick={login} type="light" />;
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        width: "100vw",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          maxWidth: "1000px",
+          alignItems: "center",
+          justifyContent: "center",
+          border: `1px solid ${variables["$gray-200"]}`,
+          margin: "4em",
+        }}
+      >
+        <div style={{ width: "50%", padding: "4em" }}>
+          <h1>Sign In</h1>
+          <p>
+            Become a volunteer - you&apos;ll be able to help mothers nationwide
+            by joining events
+          </p>
+          <GoogleButton type="light" onClick={login} />
+        </div>
+        <div style={{ width: "50%" }}>
+          <img src="/images/test.jpg" alt="image" width="100%" height="500px" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
