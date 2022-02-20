b171fec5305d4b7b6231271e5adefe4f7c49a248
jackpalaia <jack@jackpalaia.com>
2022-02-19 21:09:22 -0500

    fix auth for events tab

--- src/screens/Events/User/EventManager.jsx
+++ src/screens/Events/User/EventManager.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from "react";
 import { Button } from "reactstrap";
-import Icon from "../../../components/Icon";
-import EventTable from "./EventTable";
+import styled from "styled-components";
 import { fetchEvents } from "../../../actions/queries";
+import Icon from "../../../components/Icon";
 import { updateEvent } from "./eventHelpers";
+import EventTable from "./EventTable";
 
 const Styled = {
   Container: styled.div`
--- src/screens/PastEvents/index.jsx
+++ src/screens/PastEvents/index.jsx
@@ -1,8 +1,18 @@
+import { useSession } from "next-auth/react";
 import React from "react";
 import AdminEventManager from "./Admin";
 import UserEventManager from "./User";
 
-const EventManagerSelector = (props) =>
-  props.isAdmin || IS_ADMIN ? <AdminEventManager /> : <UserEventManager />;
+const EventManagerSelector = () => {
+  const {
+    data: { user },
+  } = useSession();
+
+  return user.role === "admin" ? (
+    <AdminEventManager user={user} />
+  ) : (
+    <UserEventManager user={user} />
+  );
+};
 
 export default EventManagerSelector;
