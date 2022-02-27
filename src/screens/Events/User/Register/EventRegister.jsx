6f897b9ac5e3912d4fa1d4107aada67a291ecdd1
jli3080 <jli3080@gatech.edu>
2022-02-26 19:27:55 -0500

    responsive and final styling

--- src/screens/Events/User/Register/EventRegister.jsx
+++ src/screens/Events/User/Register/EventRegister.jsx
@@ -48,9 +48,10 @@ const Styled = {
         background-color: ${variables["gray-100"]};
         overflow-y: scroll;
         overflow-x: hidden;
+        font-family: "Aktiv Grotesk"
     `,
     Row: styled(Row)`
-        margin-bottom: 0.2rem;
+        margin: 0 2rem 0.5rem 2rem;
     `,
     Button: styled(Button)`
         background-color: ${variables["primary"]};
@@ -66,7 +67,7 @@ const Styled = {
     `,
     EventContainer: styled(Container)`
       background-color: ${variables["white"]};
-      margin: 0.1rem;
+      margin-left: 1rem;
       border-radius: 0.5rem;
     `,
     MainText: styled.p`
@@ -74,69 +75,99 @@ const Styled = {
         font-size: 2.5rem;
         font-weight: 900;
         text-align: left;
+        overflow-wrap: break-word;
     `,
     VolunteerText: styled.p`
         color: ${variables["yiq-text-dark"]};
-        font-size: 1.5rem;
-        text-align: right;
-        font-weight: 600;
-        padding: 1rem 0 0;
+        font-size: 1.3rem;
+        font-weight: 500;
+        text-align: left;
+        padding-top: 0.5rem;
+        overflow-wrap: break-word;
     `,
     SectionText: styled.p`
         color: ${variables["yiq-text-dark"]};
         font-size: 1.3rem;
         font-weight: 900;
         text-align: left;
+        margin-left: 1rem;
+        overflow-wrap: break-word;
     `,
     TooltipText: styled.p`
         color: ${variables["primary"]};
-        font-size: 1rem;
+        font-size: 0.9rem;
         font-weight: 900;
         text-align: left;
         text-decoration: underline;
-        margin-left: 0.5rem;
+        margin-left: 0.75rem;
+        padding-top: 0.3rem;
+        overflow-wrap: break-word;
     `,
     ContactText: styled.p`
         color: ${variables["primary"]};
         font-size: 1rem;
         font-weight: 900;
         margin-left: 0.5rem;
+        text-align: left;
+        overflow-wrap: break-word;
     `,
     DetailText: styled.p`
         color: ${variables["gray-400"]};
         font-size: 0.8rem;
-        text-overflow: ellipsis;
-        overflow: hidden;
-        white-space: nowrap;
+        overflow-wrap: break-word;
     `,
     EventInfoText: styled.p`
         color: ${variables["dark"]};
         font-size: 1rem;
         font-weight: 600;
         margin-left: 0.5rem;
+        text-align: left;
+        overflow-wrap: break-word;
     `,
     EventTitleText: styled.p`
         color: ${variables["dark"]};
         font-size: 1.5rem;
         font-weight: 900;
         margin-top: 1rem;
+        margin-bottom: 0rem;
+        text-align: left;
+        overflow-wrap: break-word;
     `,
     EventRow: styled(Row)`
         margin: 0 1rem 0 1rem;
     `,
-    VolunteerNameText: styled.p`
+    SectionHeaderText: styled.p`
         color: ${variables["yiq-text-dark"]};
-        text-align: center;
+        text-align: left;
         font-weight: 900;
-        padding: 1rem 1rem 0 1rem;
+        margin-top: 0.8rem;
+        overflow-wrap: break-word;
     `,
     VolunteerInfoText: styled(Row)`
+        overflow-wrap: break-word;
     `,
     EventInfoCol: styled(Col)`
         background-color: ${variables["gray-100"]};
         margin: 0 2rem 0.5rem 0;
+        padding-top: 0.6rem;
+        padding-left: 1.2rem;
         border-radius: 0.5rem;
     `,
+    AccomodationText: styled.p`
+        color: ${variables["yiq-text-dark"]};
+        font-size: 0.8rem;
+    `,
+    AccomodationRow: styled(Row)`
+        margin: -1rem 1rem 0 3rem;
+    `,
+    VolunteerContainer: styled(Col)`
+        background-color: ${variables["white"]};
+        border-radius: 0.5rem;
+        margin-bottom: 0.5rem;
+    `,
+    VolunteerRow: styled(Row)`
+        margin-left: 1rem;
+    `
   };
 //   const EventRegister = ({id, testing}) => {
 
@@ -177,11 +208,12 @@ const EventRegister = () => {
 
     return (
             <Styled.Container fluid="md">
+                <Styled.Row></Styled.Row>
                 <Styled.Row>
-                    <Col>
+                    <Col xs="12" lg="6">
                         <Styled.MainText>Confirm Registration</Styled.MainText>
                     </Col>
-                    <Col>
+                    <Col xs="12" lg={{ size: 4, offset: 2}}>
                         {/* <h2>{event.volunteers.length}/{event.max_volunteers} Spots Remaining</h2> */}
                         <Styled.VolunteerText>30/100 Spots Remaining</Styled.VolunteerText>
                     </Col>
@@ -189,13 +221,6 @@ const EventRegister = () => {
                 <Styled.Row>
                     <Styled.SectionText>Event Information</Styled.SectionText>
                     <Styled.TooltipText>Visit Event Page</Styled.TooltipText>
-                    {/* <Col xs="6" sm="3">
-                        <Styled.SectionText>Event Information</Styled.SectionText>
-                        <Styled.TooltipText>Visit Event Page</Styled.TooltipText>
-                    </Col>
-                    <Col xs="6" sm="3">
-                        <Styled.TooltipText>Visit Event Page</Styled.TooltipText>
-                    </Col> */}
                 </Styled.Row>
                 <Styled.Row>
                     <Styled.EventContainer>
@@ -206,7 +231,7 @@ const EventRegister = () => {
                             <Styled.DetailText>Help us move boxes from our trucks to the warehouse.</Styled.DetailText>
                         </Styled.EventRow>
                         <Styled.EventRow>
-                            <Styled.EventInfoCol xs="2">
+                            <Styled.EventInfoCol xs="12" lg="3">
                                 <Row>
                                     <svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M22.8475 6.26904H7.24434C6.01329 6.26904 5.01532 7.26701 5.01532 8.49806V24.1012C5.01532 25.3322 6.01329 26.3302 7.24434 26.3302H22.8475C24.0785 26.3302 25.0765 25.3322 25.0765 24.1012V8.49806C25.0765 7.26701 24.0785 6.26904 22.8475 6.26904Z" stroke="black" stroke-width="1.88073" stroke-linecap="round" stroke-linejoin="round"/>
@@ -217,16 +242,17 @@ const EventRegister = () => {
                                     <Styled.EventInfoText>March 19</Styled.EventInfoText>
                                 </Row>
                             </Styled.EventInfoCol>
-                            <Styled.EventInfoCol xs="3">
+                            <Styled.EventInfoCol xs="12" lg="3">
                                 <Row>
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                                     <path d="M12 7V12.25L16 14" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                                     </svg>
                                     <Styled.EventInfoText>7:00AM - 4:00PM</Styled.EventInfoText>
+
                                 </Row>
                             </Styled.EventInfoCol>
-                            <Styled.EventInfoCol xs="4">
+                            <Styled.EventInfoCol xs="12" lg="4">
                                 <Row>
                                     <svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" clip-rule="evenodd" d="M14.607 25.8466C14.8442 26.115 15.2476 26.115 15.4848 25.8466C17.0928 24.0267 22.5688 17.3817 22.5688 11.2842C22.5688 7.12936 19.2007 3.76123 15.0459 3.76123C10.8911 3.76123 7.52293 7.12936 7.52293 11.2842C7.52293 17.3817 12.999 24.0267 14.607 25.8466Z" stroke="black" stroke-width="1.88073"/>
@@ -237,7 +263,10 @@ const EventRegister = () => {
                             </Styled.EventInfoCol>
                         </Styled.EventRow>
                         <Styled.EventRow>
-                            <Col>
+                            <Styled.SectionHeaderText>Contact Us</Styled.SectionHeaderText>
+                        </Styled.EventRow>
+                        <Styled.EventRow>
+                            <Col xs="12" lg="4">
                                 <Row>
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <rect x="2" y="5" width="20" height="14" rx="0.5" stroke="#EF4E79" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -248,7 +277,7 @@ const EventRegister = () => {
                                     <Styled.ContactText>helping@gmail.com</Styled.ContactText>
                                 </Row>
                             </Col>
-                            <Col>
+                            <Col xs="12" lg="4">
                                 <Row>
                                     <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M21 15.9201V18.9201C21.0011 19.1986 20.9441 19.4743 20.8325 19.7294C20.7209 19.9846 20.5573 20.2137 20.3521 20.402C20.1468 20.5902 19.9046 20.7336 19.6407 20.8228C19.3769 20.912 19.0974 20.9452 18.82 20.9201C15.7428 20.5857 12.787 19.5342 10.19 17.8501C7.77382 16.3148 5.72533 14.2663 4.18999 11.8501C2.49997 9.2413 1.44824 6.27109 1.11999 3.1801C1.095 2.90356 1.12787 2.62486 1.21649 2.36172C1.30512 2.09859 1.44756 1.85679 1.63476 1.65172C1.82196 1.44665 2.0498 1.28281 2.30379 1.17062C2.55777 1.05843 2.83233 1.00036 3.10999 1.0001H6.10999C6.5953 0.995321 7.06579 1.16718 7.43376 1.48363C7.80173 1.80008 8.04207 2.23954 8.10999 2.7201C8.23662 3.68016 8.47144 4.62282 8.80999 5.5301C8.94454 5.88802 8.97366 6.27701 8.8939 6.65098C8.81415 7.02494 8.62886 7.36821 8.35999 7.6401L7.08999 8.9101C8.51355 11.4136 10.5864 13.4865 13.09 14.9101L14.36 13.6401C14.6319 13.3712 14.9751 13.1859 15.3491 13.1062C15.7231 13.0264 16.1121 13.0556 16.47 13.1901C17.3773 13.5286 18.3199 13.7635 19.28 13.8901C19.7658 13.9586 20.2094 14.2033 20.5265 14.5776C20.8437 14.9519 21.0122 15.4297 21 15.9201Z" stroke="#EF4E79" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -260,45 +289,41 @@ const EventRegister = () => {
                     </Styled.EventContainer>
                 </Styled.Row>
                 <Styled.Row>
-                    <Container>
-                        <Row>
-                            <Styled.SectionText>Your Group</Styled.SectionText>
-                            <Styled.TooltipText>Add Minor (under 13 years old)</Styled.TooltipText>
-                        </Row>
-                        <Row>
-                            <FormGroup check>
-                                <Input type="checkbox" />{' '}
-                            </FormGroup>
-                            <h6>I require accomadation for my court required hours</h6>
-                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10 13.3333L10 9.16659" stroke="#EF4E79" stroke-width="1.25" stroke-linecap="round"/>
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M10 5.83333C9.53976 5.83333 9.16666 6.20643 9.16666 6.66667C9.16666 7.1269 9.53976 7.5 10 7.5C10.4602 7.5 10.8333 7.1269 10.8333 6.66667C10.8333 6.20643 10.4602 5.83333 10 5.83333Z" fill="#EF4E79"/>
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M10 1.66659C5.39763 1.66659 1.66667 5.39755 1.66667 9.99992C1.66666 14.6023 5.39762 18.3333 10 18.3333C14.6024 18.3333 18.3333 14.6023 18.3333 9.99992C18.3333 5.39755 14.6024 1.66659 10 1.66659Z" stroke="#EF4E79" stroke-width="1.25"/>
-                            </svg>
-                        </Row>
-                        <Row>
-                            <Col xs="4">
-                                <Styled.EventContainer>
-                                    <Row>
-                                        <Styled.VolunteerNameText>Fred Burger</Styled.VolunteerNameText>
-                                    </Row>
-                                    <Row>
-                                        <Styled.DetailText>fredfredburger@gmail.com</Styled.DetailText>
-                                    </Row>
-                                </Styled.EventContainer>
-                            </Col>
-                            <Col xs="4">
-                                <Styled.EventContainer>
-                                    <Row>
-                                        <Styled.VolunteerNameText>Carlos Burger</Styled.VolunteerNameText>
-                                    </Row>
-                                    <Row>
-                                        <Styled.DetailText>Minor</Styled.DetailText>
-                                    </Row>
-                                </Styled.EventContainer>
-                            </Col>
-                        </Row>
-                    </Container>
+                    <Styled.SectionText>Your Group</Styled.SectionText>
+                    <Styled.TooltipText>Add Minor (under 13 years old)</Styled.TooltipText>
+                </Styled.Row>
+                <Styled.AccomodationRow>
+                    <FormGroup check>
+                        <Input type="checkbox" />{' '}
+                    </FormGroup>
+                    <Styled.AccomodationText>I require accomadation for my court required hours</Styled.AccomodationText>
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 13.3333L10 9.16659" stroke="#EF4E79" stroke-width="1.25" stroke-linecap="round"/>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M10 5.83333C9.53976 5.83333 9.16666 6.20643 9.16666 6.66667C9.16666 7.1269 9.53976 7.5 10 7.5C10.4602 7.5 10.8333 7.1269 10.8333 6.66667C10.8333 6.20643 10.4602 5.83333 10 5.83333Z" fill="#EF4E79"/>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M10 1.66659C5.39763 1.66659 1.66667 5.39755 1.66667 9.99992C1.66666 14.6023 5.39762 18.3333 10 18.3333C14.6024 18.3333 18.3333 14.6023 18.3333 9.99992C18.3333 5.39755 14.6024 1.66659 10 1.66659Z" stroke="#EF4E79" stroke-width="1.25"/>
+                    </svg>
+                </Styled.AccomodationRow>
+                <Styled.Row>            
+                    <Col xs="12" lg="3">
+                        <Styled.VolunteerContainer>
+                            <Styled.VolunteerRow>
+                                <Styled.SectionHeaderText>Fred Burger</Styled.SectionHeaderText>
+                            </Styled.VolunteerRow>
+                            <Styled.VolunteerRow>
+                                <Styled.DetailText>fredfredburger@gmail.com</Styled.DetailText>
+                            </Styled.VolunteerRow>
+                        </Styled.VolunteerContainer>
+                    </Col>
+                    <Col xs="12" lg="3">
+                        <Styled.VolunteerContainer>
+                            <Styled.VolunteerRow>
+                                <Styled.SectionHeaderText>Carlos Burger</Styled.SectionHeaderText>
+                            </Styled.VolunteerRow>
+                            <Styled.VolunteerRow>
+                                <Styled.DetailText>Minor</Styled.DetailText>
+                            </Styled.VolunteerRow>
+                        </Styled.VolunteerContainer>
+                    </Col>
                 </Styled.Row>  
                 <Styled.ModalFooter>
                     <Styled.Button onClick={() => onCompleteRegistration()}>Complete Registration</Styled.Button>
