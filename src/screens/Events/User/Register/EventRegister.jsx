87f39e84f6081b294cfe6e4af3d727512dfffbe2
jli3080 <jli3080@gatech.edu>
2022-03-17 23:45:03 -0400

    modal components and ui

--- src/screens/Events/User/Register/EventRegister.jsx
+++ src/screens/Events/User/Register/EventRegister.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import { useSession } from "next-auth/react";
 import styled from "styled-components";
 import { ModalFooter, Row, Col, Button, Container, FormGroup, UncontrolledTooltip, Input } from "reactstrap";
 import EventRegisterInfoContainer from "./EventRegisterInfoContainer";
+import EventMinorModal from "./EventMinorModal";
+import EventWaiverModal from "./EventWaiverModal";
 import IconSpecial from "../../../../components/IconSpecial";
+import { fetchEvents } from "../../../../actions/queries";
 
 import PropTypes from "prop-types";
 import variables from "../../../../design-tokens/_variables.module.scss";
@@ -30,6 +34,9 @@ const Styled = {
         position: sticky;
         bottom: 0;
         margin: 0 -1rem 0 -1rem;
+    `,
+    Title: styled.div`
+        margin-top: 2rem;
     `,
     MainText: styled.p`
         color: ${variables["yiq-text-dark"]};
@@ -63,6 +70,10 @@ const Styled = {
         text-align: left;
         margin-left: 1rem;
         overflow-wrap: break-word;
+        margin-right: 1rem;
+    `,
+    LinkedButton: styled(Button) `
+        background-color: transparent;
     `,
     LinkedText: styled.p`
         color: ${variables["primary"]};
@@ -70,9 +81,16 @@ const Styled = {
         font-weight: 900;
         text-align: left;
         text-decoration: underline;
-        margin-left: 1rem;
         padding-top: 0.4rem;
         overflow-wrap: break-word;
+    `,
+    LinkedTextRight: styled.p`
+        color: ${variables["danger"]};
+        font-size: 0.9rem;
+        font-weight: 900;
+        text-align: right;
+        text-decoration: underline;
+        padding-top: 0.4rem;
     `,
     DetailText: styled.p`
         color: ${variables["gray-400"]};
@@ -104,31 +122,143 @@ const Styled = {
     VolunteerRow: styled(Row)`
         margin-left: 1rem;
     `,
+    CheckGif: styled.img`
+        width: 10rem;
+        height: 10rem;
+    `,
+    LoadGif: styled.img`
+        display: block;
+        margin-left: auto;
+        margin-right: auto;        
+        width: 15rem;
+        height: 15rem;
+        margin-top: -3rem;
+    `,
+    EventContainer: styled(Container)`
+        background-color: ${variables["white"]};
+        border-radius: 0.5rem;
+        height: 100%;
+        width: 90%;
+    `,
   };
 
 const EventRegister = (event) => {
     const router = useRouter();
     const { data: session } = useSession();
     const user = session.user;
+    const [showMinorModal, setShowMinorModal] = useState(false);
+    const [showWaiverModal, setShowWaiverModal] = useState(false);
+    const [hasMinor, setHasMinor] = useState(false);
+    const [isRegistered, setIsRegistered] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
-    const onCompleteRegistration = () => {
-        router.replace('/events')
+    const onRefresh = () => {
+        setIsLoading(true);
+        fetchEvents()
+        .then()
+        .finally(() => {
+            setTimeout(() => setIsLoading(false), 7000)
+        });
+    };
+
+    const onCompleteRegistrationClicked = () => {
+        setShowWaiverModal(true);
+    }
+
+    const onAddMinorClicked = () => {
+        setShowMinorModal(true);
+    };
+
+    const onReturnToHomeClicked = () => {
+        router.replace("/");
+    }
+
+    const onRegisterAfterWaiverClicked = () => {
+        toggleWaiverModal();
+        onRefresh();
+        setIsRegistered(true);
+    }
+
+    const toggleMinorModal = () => {
+        setShowMinorModal((prev) => !prev);
+    };
+
+    const toggleWaiverModal = () => {
+        setShowWaiverModal((prev) => !prev);
+    }
+
+    const toggleHasMinor = () => {
+        setHasMinor((prev) => !prev);
     }
 
     return (
         <Styled.Container fluid="md">
-            <Styled.Row></Styled.Row>
-            <Styled.Row>
-                <Col xs="12" lg="6">
-                    <Styled.MainText>Confirm Registration</Styled.MainText>
-                </Col>
-                <Col xs="12" lg={{ size: 4, offset: 2}}>
-                    <Row>
-                        <Styled.VolunteerNumberText>30/100 </Styled.VolunteerNumberText>
-                        <Styled.VolunteerText> Spots Remaining</Styled.VolunteerText>
-                    </Row>
-                </Col>
-            </Styled.Row>
+            <Styled.Title/>
+            { 
+                !isRegistered && 
+                <Styled.Row>
+                    <Col xs="12" lg="6">
+                        <Styled.MainText>Confirm Registration</Styled.MainText>
+                    </Col>
+                    <Col xs="12" lg={{ size: 4, offset: 2}}>
+                        <Row>
+                            <Styled.VolunteerNumberText>30/100 </Styled.VolunteerNumberText>
+                            <Styled.VolunteerText> Spots Remaining</Styled.VolunteerText>
+                        </Row>
+                    </Col>
+                </Styled.Row> 
+            }
+            { 
+                isRegistered && !isLoading &&
+                <React.Fragment>
+                <Styled.Row>
+                    <Col xs="12" lg="6">
+                        <Styled.MainText>Registration Confirmed</Styled.MainText>
+                    </Col>
+                </Styled.Row>
+                <Styled.Row>
+                    <Col xs="12" lg="2">
+                        <Styled.CheckGif src="/images/check.gif" alt="check" />
+                    </Col>
+                    <Col xs="12" lg="10">
+                        <Styled.EventContainer>
+                            <Styled.Row/>
+                            <Styled.Row/>
+                            <Styled.Row>
+                                <Styled.MainText>We’ve sent a confirmation email. See you soon!</Styled.MainText>
+                            </Styled.Row>
+                            <Styled.Row>
+                                <Button color="primary" onClick={onReturnToHomeClicked}>
+                                Return to Home
+                                </Button>
+                            </Styled.Row>
+                            <Styled.Row style={{flex: 1}}>
+                                <Styled.LinkedText>Resend Confirmation</Styled.LinkedText>
+                                <Styled.SectionText/>
+                                <Styled.LinkedText>View Waivers</Styled.LinkedText>
+                                <Styled.SectionText/>
+                                <Styled.LinkedTextRight>Cancel Registration</Styled.LinkedTextRight>
+                            </Styled.Row>
+                        </Styled.EventContainer>
+                        
+                    </Col>
+                </Styled.Row>
+                </React.Fragment>
+            }
+            {
+                isLoading &&
+                <React.Fragment>
+                    <Styled.Row>
+                        <Col xs="12" lg="6">
+                            <Styled.MainText>Confirming...</Styled.MainText>
+                        </Col>
+                    </Styled.Row>
+                    <Col xs="12">
+                        <Styled.LoadGif src="/images/loading.gif" alt="loading" />
+                    </Col>
+                </React.Fragment>
+            }
+            <Styled.Row/>
             <Styled.Row>
                 <Styled.SectionText>Event Information</Styled.SectionText>
                 <Styled.LinkedText>Visit Event Page</Styled.LinkedText>
@@ -138,11 +268,13 @@ const EventRegister = (event) => {
             </Styled.Row>
             <Styled.Row>
                 <Styled.SectionText>Your Group</Styled.SectionText>
-                <Styled.LinkedText>Add Minor (under 13 years old)</Styled.LinkedText>
+                <Link href="/register">
+                    <Styled.LinkedText onClick={ onAddMinorClicked }>Add Minor (under 13 years old)</Styled.LinkedText>
+                </Link>
             </Styled.Row>
             <Styled.AccomodationRow>
                 <FormGroup check>
-                    <Input type="checkbox"/>{' '}
+                    <Input type="checkbox" onChange={ toggleHasMinor }/>{' '}
                 </FormGroup>
                 <Styled.AccomodationText>I require accomadation for my court required hours</Styled.AccomodationText>
                 <IconSpecial width="20" height="20" viewBox="0 0 20 20" name="info" href="#" id="tooltipShow"/>
@@ -172,11 +304,23 @@ const EventRegister = (event) => {
                     </Styled.VolunteerContainer>
                 </Col>
             </Styled.Row>  
-            <Styled.ModalFooter>
-                <Styled.Button onClick={() => onCompleteRegistration()}>Complete Registration</Styled.Button>
-            </Styled.ModalFooter>  
+            {
+                !isRegistered && 
+                <Styled.ModalFooter>
+                    <Styled.Button onClick={onCompleteRegistrationClicked}>Complete Registration</Styled.Button>
+                </Styled.ModalFooter> 
+            }
+            <EventMinorModal
+                open={showMinorModal}
+                toggle={toggleMinorModal}
+            />
+            <EventWaiverModal
+                open={showWaiverModal}
+                toggle={toggleWaiverModal}
+                hasMinor={hasMinor}
+                onRegisterAfterWaiverClicked={onRegisterAfterWaiverClicked}
+            />
         </Styled.Container>
-
     )
 }
 
