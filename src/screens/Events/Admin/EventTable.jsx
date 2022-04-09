641fc001dd2884166626fa55daa5ec26c810f5b1
Shweta <35878561+shwetamurali@users.noreply.github.com>
2022-04-09 19:17:58 -0400

    minor ui changes to event cell

--- src/screens/Events/Admin/EventTable.jsx
+++ src/screens/Events/Admin/EventTable.jsx
@@ -13,63 +13,76 @@ const Styled = {
   `,
   Container: styled.div`
     width: 100%;
-    height:100%;
-    margin:auto;
+    height: 100%;
+    margin: auto;
   `,
   ul: styled.ul`
-    list-style-type:none;
+    list-style-type: none;
   `,
   List: styled.li`
-    padding-bottom:120px;
+    padding-bottom: 120px;
   `,
 };
 
 const convertTime = (time) => {
-  let [hour, min] = time.split(':');
+  let [hour, min] = time.split(":");
   let hours = parseInt(hour);
   let suffix = time[-2];
-  if (!(suffix in ['pm', 'am', 'PM', 'AM'])) {
-    suffix = (hours > 11)? 'pm': 'am';
+  if (!(suffix in ["pm", "am", "PM", "AM"])) {
+    suffix = hours > 11 ? "pm" : "am";
   }
-  hours = ((hours + 11) % 12 + 1);
-  return hours.toString()+':'+min+suffix;
-}
+  hours = ((hours + 11) % 12) + 1;
+  return hours.toString() + ":" + min + suffix;
+};
 
 const getMinorTotal = (minors) => {
   let total = 0;
-  minors.forEach(minorObj => {
+  minors.forEach((minorObj) => {
     total += minorObj.minor.length;
   });
   return total;
-}
+};
 
-const EventTable = ({ events, onEditClicked, onDeleteClicked}) => {
+const EventTable = ({ events, onEditClicked, onDeleteClicked }) => {
   return (
     <Styled.Container>
       <Styled.ul>
         {events.map((event) => (
           <Styled.List>
-            <Link href={`events/${event._id}`}><Table.EventList>
-              <Table.Inner>
-                <Table.Slots>SLOTS</Table.Slots>
-                <Table.Edit>
-                  <Styled.Button onClick={() => onEditClicked(event)}>
-                    <Icon color="grey3" name="create" />
-                  </Styled.Button>
-                </Table.Edit>
-                <Table.Delete>
-                  <Styled.Button onClick={() => onDeleteClicked(event)}>
-                    <Icon color="grey3" name="delete" />
-                  </Styled.Button>
-                </Table.Delete>
-                <Table.Text>
-                  <Table.EventName>{event.title}</Table.EventName>
-                  <Table.Volunteers>{event.volunteers.length + getMinorTotal(event.minors)}/{event.max_volunteers}          </Table.Volunteers>
-                  <Table.Time>{convertTime(event.startTime)} - {convertTime(event.endTime)}</Table.Time>
-                </Table.Text>
-              </Table.Inner>
-              <Table.Creation>{event.date.slice(0,10)}</Table.Creation>
-            </Table.EventList></Link>
+            <Link href={`events/${event._id}`}>
+              <Table.EventList>
+                <Table.Inner>
+                  <Table.Edit>
+                    <Styled.Button onClick={() => onEditClicked(event)}>
+                      <Icon color="grey3" name="create" />
+                    </Styled.Button>
+                  </Table.Edit>
+                  <Table.Delete>
+                    <Styled.Button onClick={() => onDeleteClicked(event)}>
+                      <Icon color="grey3" name="delete" />
+                    </Styled.Button>
+                  </Table.Delete>
+                  <Table.Text>
+                    <Table.EventName>{event.title}</Table.EventName>
+                    <Table.Volunteers>
+                      {" "}
+                      {event.max_volunteers -
+                        event.volunteers.length +
+                        getMinorTotal(event.minors)}{" "}
+                      slots available
+                    </Table.Volunteers>
+                    <Table.Time>
+                      {convertTime(event.startTime)} -{" "}
+                      {convertTime(event.endTime)} EST
+                    </Table.Time>
+                  </Table.Text>
+                </Table.Inner>
+                <Table.Creation>
+                  {event.date.slice(5, 7)}/{event.date.slice(8, 10)}/
+                  {event.date.slice(0, 4)}{" "}
+                </Table.Creation>
+              </Table.EventList>
+            </Link>
           </Styled.List>
         ))}
       </Styled.ul>
@@ -80,6 +93,6 @@ EventTable.propTypes = {
   events: PropTypes.Array,
   onEditClicked: PropTypes.func,
   onDeleteClicked: PropTypes.func,
-}
+};
 
 export default EventTable;
