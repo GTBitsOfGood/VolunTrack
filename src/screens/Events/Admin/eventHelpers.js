93bd351c609d0f34436cddbe1f8fca072f78b8ab
Shweta <35878561+shwetamurali@users.noreply.github.com>
2022-09-15 21:46:55 -0400

    update backend

--- src/screens/Events/Admin/eventHelpers.js
+++ src/screens/Events/Admin/eventHelpers.js
@@ -10,4 +10,12 @@ export const eventValidator = object().shape({
   zip: string().trim().required(),
   max_volunteers: string().trim().required(),
   description: string().trim().required(),
+  pocName: string().trim().required(),
+  pocEmail: string().trim().required(),
+  pocPhone: string().trim().required(),
+  addressLineOne: string().trim().required(),
+  addressLineTwo: string().trim(),
+  orgCity: string().trim().required(),
+  orgState: string().trim().required(),
+  orgZip: string().trim().required(),
 });
