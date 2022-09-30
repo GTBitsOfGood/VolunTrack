9f41fc4c0c196a386a93ba0bea43bffb794fe05e
Shweta <35878561+shwetamurali@users.noreply.github.com>
2022-09-30 01:30:36 -0400

    save to db

---
+++ src/pages/api/settings/updateInvitedAdmin.js
@@ -0,0 +1,15 @@
+const { updateAdmin } = require("../../../../server/actions/settings");
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    let result = await updateAdmin(
+      req.query.email
+    );
+
+    if (result.status === 200) {
+      res.status(200).send();
+    } else {
+      res.status(result.status).json(result.message);
+    }
+  }
+}
