7dfd7f52d6f693e97e70243fe162a50a73ded226
colin4554 <colin.flueck@gmail.com>
2022-09-30 10:20:51 -0400

    add function

---
+++ server/actions/settings.js
@@ -0,0 +1,4 @@
+export const updateSettings = async (email) => {
+    return { status: 400, message: "Not implemented" };
+    // return { status: 200, message: "" };
+};
--- src/pages/api/settings/updateInvitedAdmin.js
+++ src/pages/api/settings/updateInvitedAdmin.js
@@ -1,8 +1,8 @@
-const { updateAdmin } = require("../../../../server/actions/settings");
+const { updateSettings } = require("../../../../server/actions/settings");
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    let result = await updateAdmin(req.query.email);
+    let result = await updateSettings(req.query.email);
 
     if (result.status === 200) {
       res.status(200).send();
