2b9ec875b92f36bf917e5eaa39a88ed9b26a8b23
jwu649 <jwu649@gatech.edu>
2022-10-09 22:47:49 -0400

    fix lint rules error

--- src/pages/api/attendance/statistics.ts
+++ src/pages/api/attendance/statistics.ts
@@ -1,6 +1,6 @@
 import dbConnect from "../../../../server/mongodb";
 const Attendance = require("../../../../server/mongodb/models/attendance");
-const { getUserFromId } = require("../../../../server/actions/users")
+const { getUserFromId } = require("../../../../server/actions/users");
 
 export default async function handler(req, res) {
   await dbConnect();
@@ -17,21 +17,25 @@ export default async function handler(req, res) {
     console.log(attendance);
     let attendanceStats = [];
     for (let document of attendance) {
-        const response = await getUserFromId(document.userId);
-        let hours = -1;
-        if (document.timeCheckedOut != null) {
-            hours = Math.abs(document.timeCheckedOut - document.timeCheckedIn) / 36e5
-        }
-        const newDoc = {
-            userId: document.userId,
-            eventId: document.eventId,
-            name: response.message.user.bio.first_name + " " + response.message.user.bio.last_name,
-            email: response.message.user.bio.email,
-            hours: hours,
-        }
-        attendanceStats.push(newDoc);
+      const response = await getUserFromId(document.userId);
+      let hours = -1;
+      if (document.timeCheckedOut != null) {
+        hours =
+          Math.abs(document.timeCheckedOut - document.timeCheckedIn) / 36e5;
+      }
+      const newDoc = {
+        userId: document.userId,
+        eventId: document.eventId,
+        name:
+          response.message.user.bio.first_name +
+          " " +
+          response.message.user.bio.last_name,
+        email: response.message.user.bio.email,
+        hours: hours,
+      };
+      attendanceStats.push(newDoc);
     }
-    console.log(attendanceStats)
+    console.log(attendanceStats);
     return res.status(200).json(attendanceStats);
   }
 }
