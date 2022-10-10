592b33bf4114c427e08afb20c6d94260cb08acb3
colin4554 <colin.flueck@gmail.com>
2022-10-10 00:27:50 -0400

    fix build error

--- src/pages/api/users/stats.js
+++ src/pages/api/users/stats.js
@@ -1,7 +1,8 @@
-const { getEventsByUserID } = require("../../../../server/actions/volunteerstats");
+const {
+  getEventsByUserID,
+} = require("../../../../server/actions/volunteerstats");
 
 export default async function handler(req, res, next) {
-
   if (req.method === "GET") {
     let event = await getEventsByUserID(req.query.volunteer, next);
 
