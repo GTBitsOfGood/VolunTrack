60c343bab4125971390f696d8e49d7a21e5f6011
colin4554 <colin.flueck@gmail.com>
2022-10-29 12:25:16 -0400

    build fix

--- src/components/EventTable.jsx
+++ src/components/EventTable.jsx
@@ -95,14 +95,25 @@ const EventTable = ({
                   <Table.Inner>
                     <Table.EventName>{event.eventName}</Table.EventName>
 
-                    <Table.Creation>{event.timeCheckedIn.slice(0, 10)}</Table.Creation>
+                    <Table.Creation>
+                      {event.timeCheckedIn.slice(0, 10)}
+                    </Table.Creation>
 
                     <Table.Time>
                       {convertTime(event.timeCheckedIn.slice(11, 16))} -{" "}
-                      {event.timeCheckedOut == null ? "N/A" : convertTime(event.timeCheckedOut.slice(11, 16))}
+                      {event.timeCheckedOut == null
+                        ? "N/A"
+                        : convertTime(event.timeCheckedOut.slice(11, 16))}
                     </Table.Time>
                     <Table.TextInfo>
-                      &emsp;{event.timeCheckedOut == null ? "0 hour(s)" : getHours(event.timeCheckedIn.slice(11, 16), event.timeCheckedOut.slice(11, 16))} hour(s)
+                      &emsp;
+                      {event.timeCheckedOut == null
+                        ? "0 hour(s)"
+                        : getHours(
+                            event.timeCheckedIn.slice(11, 16),
+                            event.timeCheckedOut.slice(11, 16)
+                          )}{" "}
+                      hour(s)
                     </Table.TextInfo>
                   </Table.Inner>
                 </Table.EventList>
