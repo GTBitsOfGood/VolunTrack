89cb6667dc56011d66f26d6da678916e13ef570b
jwu649 <jwu649@gatech.edu>
2022-10-29 11:32:24 -0400

    add summary stats info

--- src/screens/Events/Admin/Statistics/EventStatistics.jsx
+++ src/screens/Events/Admin/Statistics/EventStatistics.jsx
@@ -1,6 +1,9 @@
 import { useRouter } from "next/router";
 import { useEffect, useMemo, useState } from "react";
-import { getAttendanceForEvent } from "../../../../actions/queries";
+import {
+  getAttendanceForEvent,
+  fetchEventsById,
+} from "../../../../actions/queries";
 import EventTable from "../../../../components/EventTable";
 import styled from "styled-components";
 import EventStatsDeleteModal from "./EventStatsDeleteModal";
@@ -16,7 +19,7 @@ const Styled = {
     flex-direction: column;
   `,
   Header: styled.h1`
-    margin: 0 2rem;
+    margin: 0 22rem;
 
     font-size: 3rem;
     font-weight: bold;
@@ -25,7 +28,6 @@ const Styled = {
     margin: 0 0 1rem 0;
 
     display: flex;
-    justify-content: space-between;
   `,
   TotalHours: styled.p`
     margin: 0 2rem;
@@ -36,16 +38,42 @@ const Styled = {
   Table: styled.div`
     margin: auto;
   `,
+  StatsContainer: styled.div`
+    width: 50%;
+    height: 50%;
+    margin: 1rem 22.5rem;
+    padding: 0.5rem;
+    background: white;
+    display: flex;
+    flex-direction: column;
+  `,
+  StatsHeader: styled.h4`
+    margin: 1rem 2rem;
+  `,
+  StatsInfoContainer: styled.div`
+    margin: 0rem 2rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  `,
+  StatsInfo: styled.div`
+    display: flex;
+    flex-direction: row;
+  `,
 };
 
 const EventStatistics = () => {
   const router = useRouter();
   const eventId = router.query.eventId;
   const [attendanceStats, setAttendanceStats] = useState([]);
+  const [event, setEvent] = useState(null);
 
   const onRefresh = () => {
     getAttendanceForEvent(eventId).then((result) => {
       setAttendanceStats(result.data);
+    });
+    fetchEventsById(eventId).then((result) => {
+      setEvent(result.data.event);
     });
   };
 
@@ -54,6 +82,9 @@ const EventStatistics = () => {
   }, []);
 
   const hours = attendanceStats.reduce((prev, curr) => prev + curr.hours, 0.0);
+  const totalVolunteers = [
+    ...new Set(attendanceStats.map((attendance) => attendance.userId)),
+  ];
   const [showEditModal, setShowEditModal] = useState(false);
   const [currEvent, setCurrEvent] = useState(null);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
@@ -79,10 +110,40 @@ const EventStatistics = () => {
     <Styled.Container>
       <Styled.HeaderRow>
         <Styled.Header>Statistics</Styled.Header>
-        <Styled.TotalHours>
-          {hours.toString().slice(0, 5)} Total Hours
-        </Styled.TotalHours>
       </Styled.HeaderRow>
+      <Styled.StatsContainer>
+        <Styled.StatsHeader>About this Event</Styled.StatsHeader>
+        {event && (
+          <Styled.StatsInfoContainer>
+            <Styled.StatsInfo>
+              <p>
+                <strong>Total Volunteers Attended: </strong>{" "}
+                {totalVolunteers.length}
+              </p>
+            </Styled.StatsInfo>
+            <Styled.StatsInfo>
+              <p>
+                <strong>Total Slots Filled: </strong> {event.volunteers.length}
+              </p>
+            </Styled.StatsInfo>
+            <Styled.StatsInfo>
+              <p>
+                <strong>Open Slots Remaining: </strong>{" "}
+                {event.max_volunteers - event.volunteers.length}
+              </p>
+            </Styled.StatsInfo>
+          </Styled.StatsInfoContainer>
+        )}
+        {event && (
+          <Styled.StatsInfoContainer>
+            <Styled.StatsInfo>
+              <p>
+                <strong>Total Hours: </strong> {hours}
+              </p>
+            </Styled.StatsInfo>
+          </Styled.StatsInfoContainer>
+        )}
+      </Styled.StatsContainer>
       <Styled.Table>
         <EventTable
           events={attendanceStats}
