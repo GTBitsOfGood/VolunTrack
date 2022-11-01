d3164fb2ee2dbec50f4a78fcc0fb3b435c95ae32
jackpalaia <jack@jackpalaia.com>
2022-10-31 22:34:45 -0400

    create HistoryEvent data model schema

---
+++ server/mongodb/models/HistoryEvent.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+// define schema
+const historyEventSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    keyword: {
+      type: String,
+      enum: ["ASSIGNED", "MODIFIED", "CREATED", "DELETED"],
+      default: "CREATED",
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+    },
+  },
+  { timestamps: true }
+);
+
+// export Event model to app
+
+module.exports =
+  mongoose.models.HistoryEvent ||
+  mongoose.model("HistoryEvent", historyEventSchema);
