30d087b35659310046b31ddcebc268fc879b8de7
jwu649 <jwu649@gatech.edu>
2022-11-03 02:33:02 -0400

    add assign user role based on invited admin list functionality

--- src/screens/Index/IndexPage.jsx
+++ src/screens/Index/IndexPage.jsx
@@ -2,13 +2,34 @@ import { useSession } from "next-auth/react";
 import router from "next/router";
 import "normalize.css";
 import React, { useEffect } from "react";
+import {
+  updateApplicantRole,
+  getInvitedAdmins,
+  removeInvitedAdmin,
+} from "../../actions/queries";
 
 const IndexPage = () => {
   const {
     data: { user },
   } = useSession();
 
+  const onRefresh = () => {
+    getInvitedAdmins().then((result) => {
+      if (result && result.data) {
+        if (result.data.includes(user.bio.email)) {
+          console.log("blah");
+          updateApplicantRole(user.bio.email, "admin").then(() => {
+            removeInvitedAdmin(user.bio.email).then(() => {
+              window.location.reload();
+            });
+          });
+        }
+      }
+    });
+  };
+
   useEffect(() => {
+    onRefresh();
     if (user.role === "admin") router.push("/events");
     if (user.role === "volunteer") router.push("/home");
   });
