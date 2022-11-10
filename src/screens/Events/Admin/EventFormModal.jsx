9c9e9adda0fd81b3225bceebf343b0879a2935e3
colin4554 <colin.flueck@gmail.com>
2022-11-10 11:19:50 -0500

    fix submit text

--- src/screens/Events/Admin/EventFormModal.jsx
+++ src/screens/Events/Admin/EventFormModal.jsx
@@ -124,7 +124,7 @@ const EventFormModal = ({ toggle, event, han, isGroupEvent }) => {
   };
 
   const emptyStringField = "";
-  const submitText = containsExistingEvent(event) ? "Submit" : "Create Event";
+  const submitText = containsExistingEvent(event) ? "Save" : "Create Event";
   const [content, setContent] = useState(
     containsExistingEvent(event) ? event.description : emptyStringField
   );
