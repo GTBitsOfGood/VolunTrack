8fea27645840baf0b7d6a949db1379b2ba371ae6
Colin Flueck <57019966+colin4554@users.noreply.github.com>
2023-02-16 11:14:55 -0500

    Add default organizationId (#262)

--- server/mongodb/models/userData.js
+++ server/mongodb/models/userData.js
@@ -42,6 +42,8 @@ const userDataSchema = new mongoose.Schema(
     organizationId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Organization",
+      default: mongoose.Types.ObjectId("63d6dcc4e1fb5fd6e69b1738"),
+
     },
   },
   {
