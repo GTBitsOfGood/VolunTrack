a93505dca3265bdcce35ec1784ecebc19395d2a0
jackpalaia <jack@jackpalaia.com>
2023-02-28 14:16:25 -0500

    add NewHeader

--- src/components/NewHeader.tsx
+++ src/components/NewHeader.tsx
@@ -1,5 +1,6 @@
-import { Avatar, Dropdown, Navbar } from "flowbite-react";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 const NewHeader: React.FC = () => {
@@ -9,47 +10,18 @@ const NewHeader: React.FC = () => {
   }: { data: any } = useSession(); // refactor to use UserType instead of any
 
   return (
-    <Navbar className="bg-black">
-      <Navbar.Brand href="https://flowbite.com/">
-        <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          className="mr-3 h-6 sm:h-9"
-          alt="Flowbite Logo"
-        />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Flowbite
-        </span>
-      </Navbar.Brand>
-      <div className="flex md:order-2">
-        <Dropdown
-          arrowIcon={false}
-          inline={true}
-          label={<Avatar img={user.imageUrl} />}
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">Bonnie Green</span>
-            <span className="block truncate text-sm font-medium">
-              name@flowbite.com
-            </span>
-          </Dropdown.Header>
-          <Dropdown.Item>Dashboard</Dropdown.Item>
-          <Dropdown.Item>Settings</Dropdown.Item>
-          <Dropdown.Item>Earnings</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
-        </Dropdown>
-        <Navbar.Toggle />
-      </div>
-      <Navbar.Collapse>
-        <Navbar.Link href="/navbars" active={true}>
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="/navbars">About</Navbar.Link>
-        <Navbar.Link href="/navbars">Services</Navbar.Link>
-        <Navbar.Link href="/navbars">Pricing</Navbar.Link>
-        <Navbar.Link href="/navbars">Contact</Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
+    <nav className="bg-white flex px-12 py-12">
+      <Link href="/home">
+        <div className="flex items-center justify-center cursor-pointer">
+          <Image
+            src="/images/helping_mamas_logo.png"
+            height="60px"
+            width="300px"
+            alt="logo"
+          />
+        </div>
+      </Link>
+    </nav>
   );
 };
 
