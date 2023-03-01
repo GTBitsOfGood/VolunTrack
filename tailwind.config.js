32463aa21c44c0ac72abcd9cfbd16ed539e062e3
Colin Flueck <57019966+colin4554@users.noreply.github.com>
2023-03-01 00:18:41 -0500

    Upgrade and Streamline Styling Dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,8 @@
 
 module.exports = {
   content: [
-    "./src/**/*.{html, js, jsx, ts, tsx}",
-    './public/**/*.{html, js, jsx, ts, tsx}"',
+    "./src/**/*.{html,js,jsx,ts,tsx}",
     "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     colors: {
