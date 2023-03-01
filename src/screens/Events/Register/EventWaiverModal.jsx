cca3ae0a93f054f67ebfde81dd3c83072fad6f1c
Tomer Shmul <45541706+ShmulTomer@users.noreply.github.com>
2023-03-01 13:36:51 -0500

    Replace PDF Waivers with Rich Text and Flowbite Components
    Co-authored-by: colin4554 <colin.flueck@gmail.com>

--- src/screens/Events/Register/EventWaiverModal.jsx
+++ src/screens/Events/Register/EventWaiverModal.jsx
@@ -1,18 +1,13 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
-import Link from "next/link";
-import {
-  Modal,
-  ModalHeader,
-  ModalFooter,
-  Button,
-  Input,
-  FormGroup,
-} from "reactstrap";
+import { Modal, ModalHeader, ModalFooter, Input, FormGroup } from "reactstrap";
+import { Button, Tabs } from "flowbite-react";
 import PropTypes from "prop-types";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { getWaivers } from "../../../actions/queries";
 import { Col, Row, Container } from "reactstrap";
 import variables from "../../../design-tokens/_variables.module.scss";
-import IconSpecial from "../../../components/IconSpecial";
 
 const Styled = {
   ModalHeader: styled(ModalHeader)`
@@ -29,11 +24,6 @@ const Styled = {
   HighlightText: styled.p`
     color: ${variables["dark"]};
   `,
-  MainButton: styled(Button)`
-    background-color: ${variables["primary"]};
-    color: ${variables["white"]};
-    width: 100%;
-  `,
   Col: styled(Col)``,
   Row: styled(Row)`
     margin: 0.5rem 2rem 0.5rem 1rem;
@@ -41,36 +31,17 @@ const Styled = {
   Text: styled.p`
     color: ${variables["yiq-text-dark"]};
   `,
-  PrimaryText: styled.p`
-    color: ${variables["primary"]};
-    text-align: center;
-    padding: 0.1rem;
-  `,
-  SecondaryTab: styled(Row)`
-    margin-left: 0.7rem;
-    border-bottom: 0.1rem solid ${variables["gray-200"]};
-    bottom: 0;
-    padding-left: 0.5rem;
-    padding-right: 0.5rem;
-    padding-top: 0.5rem;
-    height: 5%;
-  `,
-  Tab: styled(Row)`
-    margin-left: 0.7rem;
-    border: 0.1rem solid ${variables["gray-200"]};
-    border-bottom: 0;
-    bottom: 0;
-    padding-left: 0.5rem;
-    padding-right: 0.5rem;
-    padding-top: 0.5rem;
-    margin-bottom: -1rem;
-    height: 5%;
-  `,
   Container: styled(Container)`
     background-color: ${variables["success"]};
   `,
-  Button: styled(Button)`
-    margin-left: 1rem;
+  WaiverBox: styled.p`
+    border: 0.1rem solid ${variables["gray-200"]};
+    border-radius: 5px;
+    padding: 1rem;
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-bottom: 1rem;
+    font-size: 0.7rem;
   `,
 };
 
@@ -82,27 +53,15 @@ const EventWaiverModal = ({
   eventId,
   isRegistered,
 }) => {
-  const [showGuardian, setShowGuardian] = useState(true);
-
+  const {
+    data: { user },
+  } = useSession();
   const [waiverCheckboxSelected, setWaiverCheckboxSelected] = useState(false);
   const [waiverMinorCheckboxSelected, setMinorWaiverCheckboxSelected] =
     useState(false);
 
-  const onGuardianClicked = () => {
-    setShowGuardian(true);
-  };
-
-  const onMinorsClicked = () => {
-    setShowGuardian(false);
-  };
-
-  const onNextClicked = () => {
-    onMinorsClicked();
-  };
-
-  const onPrevClicked = () => {
-    onGuardianClicked();
-  };
+  const [activeTab, setActiveTab] = useState(0);
+  const tabsRef = useRef(null);
 
   const onWaiverCheckboxClicked = (e) => {
     setWaiverCheckboxSelected(e.target.checked);
@@ -112,10 +71,26 @@ const EventWaiverModal = ({
     setMinorWaiverCheckboxSelected(e.target.checked);
   };
 
-  const [showMe, setShowMe] = useState(false);
-  const togglePdf = () => {
-    setShowMe((prev) => !prev);
+  const [adultContent, setAdultContent] = useState("");
+  const [minorContent, setMinorContent] = useState("");
+
+  const loadWaivers = async () => {
+    const adult = await getWaivers("adult", user.organizationId);
+
+    if (adult.data.waiver.length > 0) {
+      setAdultContent(adult.data.waiver[0].text);
+    }
+
+    const minor = await getWaivers("minor", user.organizationId);
+
+    if (minor.data.waiver.length > 0) {
+      setMinorContent(minor.data.waiver[0].text);
+    }
   };
+
+  useEffect(() => {
+    loadWaivers();
+  }, []);
 
   return (
     <Modal isOpen={open} toggle={toggle} size="lg" centered="true">
@@ -127,232 +102,126 @@ const EventWaiverModal = ({
         )}
       </Styled.ModalHeader>
       <Styled.Row />
-      {hasMinor && isRegistered && (
-        <React.Fragment>
-          <Styled.Row>
-            {showGuardian && (
-              <React.Fragment>
-                <Link href={`/events/${eventId}/register`}>
-                  <Styled.Tab>
-                    <Styled.PrimaryText onClick={onGuardianClicked}>
-                      Guardian
-                    </Styled.PrimaryText>
-                    <IconSpecial
-                      width="8"
-                      height="8"
-                      viewBox="0 0 8 8"
-                      name="highlightTab"
-                    />
-                  </Styled.Tab>
-                </Link>
-                <Link href={`/events/${eventId}/register`}>
-                  <Styled.SecondaryTab>
-                    <Styled.PrimaryText onClick={onMinorsClicked}>
-                      Minors
-                    </Styled.PrimaryText>
-                    <IconSpecial
-                      width="8"
-                      height="8"
-                      viewBox="0 0 8 8"
-                      name="highlightTab"
-                    />
-                  </Styled.SecondaryTab>
-                </Link>
-              </React.Fragment>
-            )}
-            {!showGuardian && isRegistered && (
-              <React.Fragment>
-                <Link href={`/events/${eventId}/register`}>
-                  <Styled.SecondaryTab>
-                    <Styled.PrimaryText onClick={onGuardianClicked}>
-                      Guardian
-                    </Styled.PrimaryText>
-                    <IconSpecial
-                      width="8"
-                      height="8"
-                      viewBox="0 0 8 8"
-                      name="highlightTab"
-                    />
-                  </Styled.SecondaryTab>
-                </Link>
-                <Link href={`/events/${eventId}/register`}>
-                  <Styled.Tab>
-                    <Styled.PrimaryText onClick={onMinorsClicked}>
-                      Minors
-                    </Styled.PrimaryText>
-                    <IconSpecial
-                      width="8"
-                      height="8"
-                      viewBox="0 0 8 8"
-                      name="highlightTab"
-                    />
-                  </Styled.Tab>
-                </Link>
-              </React.Fragment>
-            )}
-          </Styled.Row>
-        </React.Fragment>
-      )}
-      {hasMinor && !isRegistered && (
-        <React.Fragment>
-          <Styled.Row>
-            <Styled.HighlightText>
-              Before you can finish registration. Please review the following
-              waivers for yourself and your accompanying minors.
-            </Styled.HighlightText>
-          </Styled.Row>
-          <Styled.Row>
-            {showGuardian && (
-              <React.Fragment>
-                <Link href={`/events/${eventId}/register`}>
-                  <Styled.Tab>
-                    <Styled.PrimaryText onClick={onGuardianClicked}>
-                      Guardian
-                    </Styled.PrimaryText>
-                    <IconSpecial
-                      width="8"
-                      height="8"
-                      viewBox="0 0 8 8"
-                      name="highlightTab"
-                    />
-                  </Styled.Tab>
-                </Link>
-                <Link href={`/events/${eventId}/register`}>
-                  <Styled.SecondaryTab>
-                    <Styled.PrimaryText onClick={onMinorsClicked}>
-                      Minors
-                    </Styled.PrimaryText>
-                    <IconSpecial
-                      width="8"
-                      height="8"
-                      viewBox="0 0 8 8"
-                      name="highlightTab"
-                    />
-                  </Styled.SecondaryTab>
-                </Link>
-              </React.Fragment>
-            )}
-            {!showGuardian && !isRegistered && (
-              <React.Fragment>
-                <Link href={`/events/${eventId}/register`}>
-                  <Styled.SecondaryTab>
-                    <Styled.PrimaryText onClick={onGuardianClicked}>
-                      Guardian
-                    </Styled.PrimaryText>
-                    <IconSpecial
-                      width="8"
-                      height="8"
-                      viewBox="0 0 8 8"
-                      name="highlightTab"
-                    />
-                  </Styled.SecondaryTab>
-                </Link>
-                <Link href={`/events/${eventId}/register`}>
-                  <Styled.Tab>
-                    <Styled.PrimaryText onClick={onMinorsClicked}>
-                      Minors
-                    </Styled.PrimaryText>
-                    <IconSpecial
-                      width="8"
-                      height="8"
-                      viewBox="0 0 8 8"
-                      name="highlightTab"
-                    />
-                  </Styled.Tab>
-                </Link>
-              </React.Fragment>
-            )}
-          </Styled.Row>
-        </React.Fragment>
-      )}
-      {!hasMinor && !isRegistered && (
-        <Styled.Row>
-          <Styled.HighlightText>
-            Before you can finish registration. Please review the following
-            waiver. <br></br>If you are under 18, the waiver must be signed by
-            your parent.
-          </Styled.HighlightText>
-        </Styled.Row>
-      )}
 
-      <Styled.Row>
-        <Styled.MainButton onClick={togglePdf}>
-          <IconSpecial width="25" height="24" viewBox="0 0 25 24" name="link" />
-          Read Waiver
-        </Styled.MainButton>
-        <Styled.Row
-          style={{
-            display: showMe ? "block" : "none",
-          }}
-        >
-          <iframe
-            style={{ width: "563px", height: "666px" }}
-            src={showGuardian ? "/files/adult.pdf" : "/files/minor.pdf"}
-            type="application/pdf"
-            title="title"
-          />
-        </Styled.Row>
-      </Styled.Row>
-      {showGuardian && !isRegistered && (
-        <Styled.Row>
-          <FormGroup check>
-            <Input
-              type="checkbox"
-              onChange={onWaiverCheckboxClicked}
-              checked={waiverCheckboxSelected}
-            />{" "}
-          </FormGroup>
-          <Styled.Text>
-            I have read the waiver and agree to its terms and conditions
-          </Styled.Text>
-        </Styled.Row>
-      )}
+      {hasMinor ? (
+        <>
+          {!isRegistered && (
+            <Styled.Row>
+              <Styled.HighlightText>
+                Before you can finish registration. Please review the following
+                waivers for yourself and your accompanying minors.
+              </Styled.HighlightText>
+            </Styled.Row>
+          )}
+          <Tabs.Group
+            style="underline"
+            ref={tabsRef}
+            onActiveTabChange={(tab) => setActiveTab(tab)}
+          >
+            <Tabs.Item title="Guardian Waiver" className="overflow-auto">
+              <Styled.WaiverBox>
+                <div dangerouslySetInnerHTML={{ __html: adultContent }} />
+              </Styled.WaiverBox>
+              {!isRegistered && (
+                <Styled.Row>
+                  <FormGroup check>
+                    <Input
+                      type="checkbox"
+                      onChange={onWaiverCheckboxClicked}
+                      checked={waiverCheckboxSelected}
+                    />{" "}
+                  </FormGroup>
+                  <Styled.Text>
+                    I have read the waiver and agree to its terms and conditions
+                  </Styled.Text>
+                </Styled.Row>
+              )}
+            </Tabs.Item>
+            <Tabs.Item title="Minor Waiver" className="overflow-auto">
+              <Styled.WaiverBox>
+                <div dangerouslySetInnerHTML={{ __html: minorContent }} />
+              </Styled.WaiverBox>
+              {!isRegistered && (
+                <Styled.Row>
+                  <FormGroup check>
+                    <Input
+                      type="checkbox"
+                      onChange={onWaiverMinorCheckboxClicked}
+                      checked={waiverMinorCheckboxSelected}
+                    />{" "}
+                  </FormGroup>
+                  <Styled.Text>
+                    I have read the waiver and agree to its terms and conditions
+                  </Styled.Text>
+                </Styled.Row>
+              )}
+            </Tabs.Item>
+          </Tabs.Group>
+        </>
+      ) : (
+        <>
+          {!isRegistered && (
+            <Styled.Row>
+              <Styled.HighlightText>
+                Before you can finish registration. Please review the following
+                waiver.
+              </Styled.HighlightText>
+            </Styled.Row>
+          )}
 
-      {!showGuardian && !isRegistered && (
-        <Styled.Row>
-          <FormGroup check>
-            <Input
-              type="checkbox"
-              onChange={onWaiverMinorCheckboxClicked}
-              checked={waiverMinorCheckboxSelected}
-            />{" "}
-          </FormGroup>
-          <Styled.Text>
-            I have read the waiver and agree to its terms and conditions
-          </Styled.Text>
-        </Styled.Row>
+          <Styled.WaiverBox>
+            <div dangerouslySetInnerHTML={{ __html: adultContent }} />
+          </Styled.WaiverBox>
+          {!isRegistered && (
+            <Styled.Row>
+              <FormGroup check>
+                <Input
+                  type="checkbox"
+                  onChange={onWaiverCheckboxClicked}
+                  checked={waiverCheckboxSelected}
+                />{" "}
+              </FormGroup>
+              <Styled.Text>
+                I have read the waiver and agree to its terms and conditions
+              </Styled.Text>
+            </Styled.Row>
+          )}
+        </>
       )}
       <ModalFooter>
         {!hasMinor && !isRegistered && (
           <Button
-            color="primary"
+            gradientMonochrome="pink"
             disabled={!waiverCheckboxSelected}
             onClick={() => onRegisterAfterWaiverClicked()}
           >
             Register
           </Button>
         )}
-        {hasMinor && !isRegistered && showGuardian && (
+        {hasMinor && !isRegistered && activeTab === 0 && (
           <Button
-            color="primary"
-            onClick={onNextClicked}
+            gradientMonochrome="pink"
+            onClick={() => tabsRef.current?.setActiveTab(1)}
             disabled={!waiverCheckboxSelected}
           >
             Next
           </Button>
         )}
-        {hasMinor && !isRegistered && !showGuardian && (
+        {hasMinor && !isRegistered && activeTab === 1 && (
           <React.Fragment>
-            <Styled.Button color="primary" onClick={onPrevClicked}>
+            <Button
+              gradientMonochrome="pink"
+              onClick={() => tabsRef.current?.setActiveTab(0)}
+            >
               Previous
-            </Styled.Button>
-            <Styled.Button
-              color="primary"
+            </Button>
+            <Button
+              gradientMonochrome="pink"
               disabled={!waiverMinorCheckboxSelected || !waiverCheckboxSelected}
               onClick={() => onRegisterAfterWaiverClicked()}
             >
               Register
-            </Styled.Button>
+            </Button>
           </React.Fragment>
         )}
       </ModalFooter>
