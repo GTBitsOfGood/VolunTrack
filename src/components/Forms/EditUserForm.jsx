8b47bac991dd2ce81f61bac0b7b798fa0027556c
cduffy8 <connorpduffy160182@gmail.com>
2023-03-05 18:05:51 -0500

    Convert into component class to add own submit dunction

--- src/components/Forms/EditUserForm.jsx
+++ src/components/Forms/EditUserForm.jsx
@@ -3,146 +3,155 @@ import { Button, Col, Container, Row } from "reactstrap";
 import * as SForm from "../../screens/sharedStyles/formStyles";
 import { Formik, Form } from "formik";
 import InputField from "./InputField";
+import React from "react";
 
-const EditUserForm = (props) => {
-  if (props.userSelectedForEdit) {
-    return (
-      <Formik
-        enableReinitialize
-        initialValues={props.userSelectedForEdit}
-        onSubmit={props.submitHandler}
-      >
-        <Form>
-          <Container>
-            <SForm.FormGroup>
-              <Row>
-                <Col>
-                  <InputField
-                    name={"first_name"}
-                    label={"First Name"}
-                    isRequired={true}
-                    disabled={false} // update based on permissions
-                    type={"text"}
-                  />
-                </Col>
-                <Col>
-                  <InputField
-                    name={"last_name"}
-                    label={"Last Name"}
-                    isRequired={true}
-                    disabled={false} // update based on permissions
-                    type={"text"}
-                  />
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <InputField
-                    name={"email"}
-                    label={"Email"}
-                    isRequired={true} // does not seem to work
-                    disabled={true} // update based on permissions
-                    type={"text"}
-                  />
-                </Col>
-                <Col>
-                  <InputField
-                    name={"phone_number"}
-                    label={"Phone"}
-                    isRequired={false}
-                    disabled={false} // update based on permissions
-                    type={"text"}
-                  />
-                </Col>
-              </Row>
-
-              <Row>
-                <Col>
-                  <InputField
-                    name={"date_of_birth"}
-                    label={"Date of Birth"}
-                    isRequired={false}
-                    disabled={false} // update based on permissions
-                    type={"text"}
-                  />
-                </Col>
-                <Col>
-                  <InputField
-                    name={"zip_code"}
-                    label={"Zip Code"}
-                    isRequired={false}
-                    disabled={false} // update based on permissions
-                    type={"text"}
-                  />
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <InputField
-                    name={"address"}
-                    label={"Address"}
-                    isRequired={false}
-                    disabled={false} // update based on permissions
-                    type={"text"}
-                  />
-                </Col>
-                <Col>
-                  <InputField
-                    name={"city"}
-                    label={"City"}
-                    isRequired={false}
-                    disabled={false} // update based on permissions
-                    type={"text"}
-                  />
-                </Col>
-                <Col>
-                  <InputField
-                    name={"state"}
-                    label={"State"}
-                    isRequired={false}
-                    disabled={false} // update based on permissions
-                    type={"text"}
-                  />
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <InputField
-                    name={"notes"}
-                    label={"Notes"}
-                    isRequired={false}
-                    disabled={false} // update based on permissions
-                    type={"text"}
-                  />
-                </Col>
-              </Row>
-            </SForm.FormGroup>
-          </Container>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "center",
-              padding: "5px",
-            }}
-          >
-            {props.isPopUp && (
-              <Button color="secondary" onClick={props.closePopUp}>
-                Cancel
-              </Button>
-            )}
-            <Button type="submit" style={{ backgroundColor: "#ef4e79" }}>
-              Update
-            </Button>
-          </div>
-        </Form>
-      </Formik>
-    );
-  } else {
-    return null;
+class EditUserForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: this.props.userSelectedForEdit,
+    };
   }
-};
+  render() {
+    if (this.props.userSelectedForEdit) {
+      return (
+        <Formik
+          enableReinitialize
+          initialValues={this.props.userSelectedForEdit}
+          onSubmit={this.props.submitHandler}
+        >
+          <Form>
+            <Container>
+              <SForm.FormGroup>
+                <Row>
+                  <Col>
+                    <InputField
+                      name={"first_name"}
+                      label={"First Name"}
+                      isRequired={true}
+                      disabled={false} // update based on permissions
+                      type={"text"}
+                    />
+                  </Col>
+                  <Col>
+                    <InputField
+                      name={"last_name"}
+                      label={"Last Name"}
+                      isRequired={true}
+                      disabled={false} // update based on permissions
+                      type={"text"}
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <InputField
+                      name={"email"}
+                      label={"Email"}
+                      isRequired={true} // does not seem to work
+                      disabled={true} // update based on permissions
+                      type={"text"}
+                    />
+                  </Col>
+                  <Col>
+                    <InputField
+                      name={"phone_number"}
+                      label={"Phone"}
+                      isRequired={false}
+                      disabled={false} // update based on permissions
+                      type={"text"}
+                    />
+                  </Col>
+                </Row>
+
+                <Row>
+                  <Col>
+                    <InputField
+                      name={"date_of_birth"}
+                      label={"Date of Birth"}
+                      isRequired={false}
+                      disabled={false} // update based on permissions
+                      type={"text"}
+                    />
+                  </Col>
+                  <Col>
+                    <InputField
+                      name={"zip_code"}
+                      label={"Zip Code"}
+                      isRequired={false}
+                      disabled={false} // update based on permissions
+                      type={"text"}
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <InputField
+                      name={"address"}
+                      label={"Address"}
+                      isRequired={false}
+                      disabled={false} // update based on permissions
+                      type={"text"}
+                    />
+                  </Col>
+                  <Col>
+                    <InputField
+                      name={"city"}
+                      label={"City"}
+                      isRequired={false}
+                      disabled={false} // update based on permissions
+                      type={"text"}
+                    />
+                  </Col>
+                  <Col>
+                    <InputField
+                      name={"state"}
+                      label={"State"}
+                      isRequired={false}
+                      disabled={false} // update based on permissions
+                      type={"text"}
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <InputField
+                      name={"notes"}
+                      label={"Notes"}
+                      isRequired={false}
+                      disabled={false} // update based on permissions
+                      type={"text"}
+                    />
+                  </Col>
+                </Row>
+              </SForm.FormGroup>
+            </Container>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                padding: "5px",
+              }}
+            >
+              {this.props.isPopUp && (
+                <Button color="secondary" onClick={this.props.closePopUp}>
+                  Cancel
+                </Button>
+              )}
+              <Button type="submit" style={{ backgroundColor: "#ef4e79" }}>
+                Update
+              </Button>
+            </div>
+          </Form>
+        </Formik>
+      );
+    } else {
+      return null;
+    }
+  }
+}
 
 EditUserForm.propTypes = {
   userSelectedForEdit: PropTypes.object.isRequired,
