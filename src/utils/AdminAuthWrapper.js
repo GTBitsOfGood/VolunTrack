4d505efacb72a0e11c7cb8cf38acd7d54c22f44f
austin-bmitchell15 <austin.bmitchell15@gmail.com>
2023-04-01 11:23:32 -0400

    305- Prettier

--- src/utils/AdminAuthWrapper.js
+++ src/utils/AdminAuthWrapper.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from "react";
 import Error from "next/error";
 import { useSession } from "next-auth/react";
 
 function AdminAuthWrapper(Component) {
-    return function WrappedComponent(props) {
-        const {
-            data: { user },
-        } = useSession();
-        if (user.role !== "admin") {
-        return (
-            <Error
-            title="You are not authorized to access this page"
-            statusCode={403}
-            />
-        );
-        } else {
-        return <Component {...props} user={user} />;
-        }
-    };
+  return function WrappedComponent(props) {
+    const {
+      data: { user },
+    } = useSession();
+    if (user.role !== "admin") {
+      return (
+        <Error
+          title="You are not authorized to access this page"
+          statusCode={403}
+        />
+      );
+    } else {
+      return <Component {...props} user={user} />;
+    }
+  };
 }
 
-export default AdminAuthWrapper
+export default AdminAuthWrapper;
