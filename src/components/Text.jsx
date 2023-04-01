ccfed5d474a268f4f751150c34d38a1d76293684
cduffy8 <connorpduffy160182@gmail.com>
2023-04-01 11:59:28 -0400

    Expand text component and implement in Auth screen

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -7,19 +7,37 @@ class Text extends React.Component {
   }
 
   render() {
-    return this.props.href ? (
-      <a
-        className={
-          "text-sm text-primaryColor underline hover:text-hoverColor " +
-          this.props.className
-        }
-        href={this.props.href}
-      >
-        {this.props.text}
-      </a>
-    ) : (
-      <p>{this.props.text}</p>
-    );
+    if (this.props.href) {
+      return (
+        <a
+          className={
+            "text-sm text-primaryColor underline hover:text-hoverColor " +
+            this.props.className
+          }
+          href={this.props.href}
+        >
+          {this.props.text}
+        </a>
+      )
+    } else {
+      let theming = "text-sm ";
+      switch (this.props.theme) {
+        case "helper":
+          theming = "text-sm text-gray "
+          break;
+        case "header":
+          return (
+            <h1 className={"text-4xl font-bold " + this.props.className}>{this.props.text}</h1>
+          );
+        case "subheading":
+          theming = "text-xl text-bold "
+          break;
+      }
+      theming = theming + this.props?.className;
+      return (
+        <p className={theming}>{this.props.text}</p>
+      );
+    }
   }
 }
 
@@ -27,6 +45,7 @@ export default Text;
 
 Text.propTypes = {
   text: PropTypes.string.isRequired,
+  theme: PropTypes.string,
   // children: PropTypes.arrayOf(PropTypes.element),
   className: PropTypes.string,
   href: PropTypes.string,
