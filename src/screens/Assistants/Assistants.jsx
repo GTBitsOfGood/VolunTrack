a75c68dfd801ec3c82d7a1612da380e8623ed922
austin-bmitchell15 <austin.bmitchell15@gmail.com>
2023-04-01 11:18:42 -0400

    305- tailwind styling for most of the assistants page

--- src/screens/Assistants/Assistants.jsx
+++ src/screens/Assistants/Assistants.jsx
@@ -32,36 +32,6 @@ import AdminAuthWrapper from "../../utils/AdminAuthWrapper";
 const PAGE_SIZE = 10;
 
 const Styled = {
-  Container: styled.div`
-    width: 100%;
-    height: 100%;
-
-    padding-top: 1rem;
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    left: 10%;
-  `,
-  PaginationContainer: styled.div`
-    background: white;
-    border-radius: 0.5rem;
-    display: flex;
-    align-items: center;
-    height: 3rem;
-
-    p {
-      color: hsl(0, 0%, 50%);
-      margin: 0 1rem;
-      font-size: 1.2rem;
-    }
-  `,
-  ButtonContainer: styled.div`
-    width: 95%;
-    maxwidth: 80rem;
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 1rem;
-  `,
   Col: styled(Col)`
     margin-top: 0.5rem;
   `,
@@ -76,35 +46,7 @@ const Styled = {
     border: 1px solid lightgray;
     border-radius: 0.5rem;
   `,
-  TableUsers: styled.div`
-    width: 80%;
-  `,
-  Text: styled.div`
-    color: #000000;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 32px;
-    line-height: 41px;
-  `,
 };
-
-function authWrapper(Component) {
-  return function WrappedComponent(props) {
-    const {
-      data: { user },
-    } = useSession();
-    if (user.role !== "admin") {
-      return (
-        <Error
-          title="You are not authorized to access this page"
-          statusCode={403}
-        />
-      );
-    } else {
-      return <Component {...props} user={user} />;
-    }
-  };
-}
 
 class Assistants extends React.Component {
   state = {
@@ -216,9 +158,9 @@ class Assistants extends React.Component {
   render() {
     const { loadingMoreUsers } = this.state;
     return (
-      <Styled.Container>
+      <div className="flex flex-col pt-4 relative w-full h-full left-[10%] justify-center">
         <Styled.Row>
-          <Styled.Text>Employees</Styled.Text>
+          <div className="text-black not-italic font-bold text-4xl">Employees</div>
         </Styled.Row>
         <Styled.Row>
           <Styled.Col>
@@ -233,7 +175,7 @@ class Assistants extends React.Component {
           </Styled.Col>
         </Styled.Row>
         <Styled.Row>
-          <Styled.TableUsers>
+          <div className="w-[80%]">
             <AssistantTable
               sessionUser={this.props.user}
               users={this.getUsersAtPage()}
@@ -244,7 +186,7 @@ class Assistants extends React.Component {
               deletePendingCallback={this.onDeletePending}
               deleteUserCallback={this.onDeleteUser}
             />
-          </Styled.TableUsers>
+          </div>
         </Styled.Row>
         <Formik
           initialValues={{
@@ -296,7 +238,7 @@ class Assistants extends React.Component {
             </Modal>
           )}
         </Formik>
-      </Styled.Container>
+      </div>
     );
   }
 }
