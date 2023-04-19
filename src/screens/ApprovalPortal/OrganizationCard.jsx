86816a1a2b031689114fd4872b522625c7b3eee2
bphoebew <bphoebew@gmail.com>
2023-04-19 13:27:25 -0400

    approval card delete border

--- src/screens/ApprovalPortal/OrganizationCard.jsx
+++ src/screens/ApprovalPortal/OrganizationCard.jsx
@@ -8,7 +8,7 @@ const OrganizationCard = (props) => {
   };
 
   return (
-    <Card className="w-full bg-grey">
+    <Card className="w-full border-0 bg-grey">
       <div className="flex justify-center">
         <div className="flex">
           <div className="mr-2 font-semibold">Status:</div>
