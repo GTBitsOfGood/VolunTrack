038dced3117e66a00a34ec78b54b2091da1bf516
cduffy8 <connorpduffy160182@gmail.com>
2023-04-24 18:49:50 -0400

    prettier

--- src/screens/Events/Admin/Attendance/AttendanceFunctionality.jsx
+++ src/screens/Events/Admin/Attendance/AttendanceFunctionality.jsx
@@ -57,48 +57,63 @@ const AttendanceFunctionality = ({
         </Table.Body>
       </Table>
     )}
-    {waitingVolunteers?.length === 0 && (<Text text="Congratulations! All volunteers were checked in!" className="text-primaryColor flex justify-center" type="subheader"/>)}
+    {waitingVolunteers?.length === 0 && (
+      <Text
+        text="Congratulations! All volunteers were checked in!"
+        className="flex justify-center text-primaryColor"
+        type="subheader"
+      />
+    )}
 
     <Styled.InfoText className="font-bold text-black">
       Check-Out
     </Styled.InfoText>
-    {checkedInVolunteers?.length > 0 || checkedOutVolunteers?.length > 0 &&
-    (<Table>
-      <Table.Head>
-        <Table.HeadCell>
-          <span className="sr-only">Check-Out</span>
-        </Table.HeadCell>
-        <Table.HeadCell>Name</Table.HeadCell>
-        <Table.HeadCell>Email</Table.HeadCell>
-        <Table.HeadCell>Phone</Table.HeadCell>
-        <Table.HeadCell>Status</Table.HeadCell>
-      </Table.Head>
-      <Table.Body className="divide-y">
-        {checkedInVolunteers?.length > 0 &&
-          checkedInVolunteers.map((volunteer) => (
-            <Volunteer
-              key={volunteer._id}
-              volunteer={volunteer}
-              minors={minors[volunteer._id]}
-              onClick={checkOut}
-              checkInStatus={"checkedIn"}
-              isEnded={isEnded}
-            />
-          ))}
-        {checkedOutVolunteers?.length > 0 &&
-          checkedOutVolunteers.map((volunteer) => (
-            <Volunteer
-              key={volunteer._id}
-              volunteer={volunteer}
-              minors={minors[volunteer._id]}
-              onClick={checkOut}
-              checkInStatus={"checkedOut"}
-              isEnded={isEnded}
-            />
-          ))}
-      </Table.Body>
-    </Table>)}
-    {checkedInVolunteers?.length === 0 || checkedOutVolunteers?.length === 0 && (<Text text="No one has been checked out yet." className="text-primaryColor flex justify-center" type="subheader"/>)}
+    {checkedInVolunteers?.length > 0 ||
+      (checkedOutVolunteers?.length > 0 && (
+        <Table>
+          <Table.Head>
+            <Table.HeadCell>
+              <span className="sr-only">Check-Out</span>
+            </Table.HeadCell>
+            <Table.HeadCell>Name</Table.HeadCell>
+            <Table.HeadCell>Email</Table.HeadCell>
+            <Table.HeadCell>Phone</Table.HeadCell>
+            <Table.HeadCell>Status</Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {checkedInVolunteers?.length > 0 &&
+              checkedInVolunteers.map((volunteer) => (
+                <Volunteer
+                  key={volunteer._id}
+                  volunteer={volunteer}
+                  minors={minors[volunteer._id]}
+                  onClick={checkOut}
+                  checkInStatus={"checkedIn"}
+                  isEnded={isEnded}
+                />
+              ))}
+            {checkedOutVolunteers?.length > 0 &&
+              checkedOutVolunteers.map((volunteer) => (
+                <Volunteer
+                  key={volunteer._id}
+                  volunteer={volunteer}
+                  minors={minors[volunteer._id]}
+                  onClick={checkOut}
+                  checkInStatus={"checkedOut"}
+                  isEnded={isEnded}
+                />
+              ))}
+          </Table.Body>
+        </Table>
+      ))}
+    {checkedInVolunteers?.length === 0 ||
+      (checkedOutVolunteers?.length === 0 && (
+        <Text
+          text="No one has been checked out yet."
+          className="flex justify-center text-primaryColor"
+          type="subheader"
+        />
+      ))}
   </>
 );
 export default AttendanceFunctionality;
