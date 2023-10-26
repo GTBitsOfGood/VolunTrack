3010c744b40093c7b5c5ff899d27085187a7f424
Hamza Qadri <syedhamzaq@gmail.com>
2023-10-26 14:03:54 -0400

    Move landing page to screens, features to module

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -3,7 +3,7 @@ import router from "next/router";
 import PropTypes from "prop-types";
 import ResetPage from "../pages/passwordreset/[resetCode]";
 import AuthPage from "../screens/Auth";
-import LandingPage from "../components/LandingPage";
+import LandingPage from "../screens/LandingPage";
 import AddOrganizationModal from "../components/AddOrganizationModal";
 
 // AuthProvider wraps the entire application and makes sure only authenticated users can access the app
---
+++ src/screens/LandingPage/features.ts
@@ -0,0 +1,20 @@
+export const features = [
+  {
+    title: "Track your impact",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    imageUrl: "/images/landing-feature-steps.svg",
+  },
+  {
+    title: "Plan memorable events",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    imageUrl: "/images/landing-feature-schedule.svg",
+  },
+  {
+    title: "Track your impact",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    imageUrl: "/images/landing-feature-steps.svg",
+  },
+];
