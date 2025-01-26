5bb1384aa99084d3284ee86e5dd5453ef5559b1a
Bossixd <bosspattakit123@gmail.com>
2025-01-25 22:55:59 -0500

    edited task UI, added edit event capabilities

--- src/screens/Events/Admin/EventFormModal.jsx
+++ src/screens/Events/Admin/EventFormModal.jsx
@@ -281,42 +281,73 @@ const EventFormModal = ({
 
   const [editingTask, setEditingTask] = useState(false);
   const [taskName, setTaskName] = useState("");
-  const [lastTaskIndex, setLastTaskIndex] = useState(-1);
   const [tasks, setTasks] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1);
 
   const addTask = () => {
     setEditingTask(true);
+    setEditIndex(-1);
+    setTaskName("");
   };
 
   const closeTask = () => {
     setEditingTask(false);
     setTaskName("");
-    setLastTaskIndex(-1);
+    setEditIndex(-1);
   };
 
   const editTask = (index) => {
     setTaskName(tasks[index]);
-    setLastTaskIndex(index);
-    addTask();
-  }
+    setEditIndex(index);
+    setEditingTask(true);
+  };
 
   const saveTask = (values, setFieldValue) => {
-    if (lastTaskIndex === -1) {
-      setFieldValue("eventParent.tasks", [...values.eventParent.tasks, taskName]);
+    if (taskName === "") {
+      closeTask();
+      return;
+    }
+    if (editIndex === -1) {
+      setFieldValue("eventParent.tasks", [
+        ...values.eventParent.tasks,
+        taskName,
+      ]);
       setTasks([...values.eventParent.tasks, taskName]);
     } else {
-      setFieldValue("eventParent.tasks", [...(values.eventParent.tasks.slice(0, lastTaskIndex)), taskName, ...(values.eventParent.tasks.slice(lastTaskIndex + 1))]);
-      setTasks([...values.eventParent.tasks.slice(0, lastTaskIndex), taskName, ...values.eventParent.tasks.slice(lastTaskIndex + 1)]);
+      setFieldValue("eventParent.tasks", [
+        ...values.eventParent.tasks.slice(0, editIndex),
+        taskName,
+        ...values.eventParent.tasks.slice(editIndex + 1),
+      ]);
+      setTasks([
+        ...values.eventParent.tasks.slice(0, editIndex),
+        taskName,
+        ...values.eventParent.tasks.slice(editIndex + 1),
+      ]);
     }
     closeTask();
   };
 
+  const deleteTask = (index, setFieldValue) => {
+    setFieldValue("eventParent.tasks", [
+      ...tasks.slice(0, index),
+      ...tasks.slice(index + 1),
+    ]);
+    setTasks([...tasks.slice(0, index), ...tasks.slice(index + 1)]);
+    closeTask();
+  };
+
   const readTasks = (values) => {
-    console.log(values);
     if (values?.eventParent?.tasks) {
       setTasks(values.eventParent.tasks);
     }
-  }
+  };
+
+  useEffect(() => {
+    if (event?.eventParent?.tasks) {
+      setTasks(event.eventParent.tasks);
+    }
+  }, []);
 
   /* -------- */
 
@@ -728,8 +759,7 @@ const EventFormModal = ({
                   )}
                 </FormGroup>
               </Styled.Row>
-              <div className="flex flex-row"
-              onLoad={() => readTasks(values)}>
+              <div className="flex flex-row" on={() => readTasks(values)}>
                 <Label class="mb-1 h-6 font-medium text-slate-600">Tasks</Label>
               </div>
               <Styled.Row>
@@ -737,34 +767,96 @@ const EventFormModal = ({
                   <Dropdown
                     inline={true}
                     arrowIcon={false}
-                    label={
-                      <BoGButton
-                        text={
-                          taskName !== ""
-                            ? taskName
-                            : "Select Task"
-                        }
-                        dropdown={true}
-                      />
-                    }
+                    enableTypeAhead={false}
+                    dismissOnClick={false}
+                    label={<BoGButton text={"Select Task"} dropdown={true} />}
                   >
                     {tasks.map((task, index) => (
-                      <Dropdown.Item
-                        id={task}
-                        onClick={() => {
-                          editTask(index);
-                        }}
-                      >
-                        {task}
-                      </Dropdown.Item>
+                      <>
+                        {editIndex !== index && (
+                          <Dropdown.Item id={task}>
+                            <div className="flex w-48 flex-row justify-between">
+                              <div
+                              className="w-full"
+                                onClick={() => {
+                                  editTask(index);
+                                }}
+                              >
+                                {task}
+                              </div>
+                              <div
+                                className="border-l border-black pl-1"
+                                onClick={() => deleteTask(index, setFieldValue)}
+                              >
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  width={18}
+                                  viewBox="0 0 24 24"
+                                  strokeWidth={1.5}
+                                  stroke="currentColor"
+                                  className="size-6"
+                                >
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                  />
+                                </svg>
+                              </div>
+                            </div>
+                          </Dropdown.Item>
+                        )}
+                        {editIndex === index && (
+                          <div className="flex w-full justify-center">
+                            <TextInput
+                              class="mt-0 rounded-md border-gray-300 bg-white disabled:border-gray-500 disabled:bg-gray-300"
+                              id="taskName"
+                              name="taskName"
+                              value={taskName}
+                              autoFocus={true}
+                              onChange={(e) => {
+                                setTaskName(e.target.value);
+                              }}
+                              onKeyDown={(e) => {
+                                if (e.key === "Enter") {
+                                  saveTask(values, setFieldValue);
+                                }
+                              }}
+                              type="text"
+                              placeholder="Your task name"
+                            />
+                          </div>
+                        )}
+                      </>
                     ))}
-                    <Dropdown.Item
-                      onClick={addTask}
-                    >
-                      + Add Events
-                    </Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Header>
+                      {(!editingTask || editIndex !== -1) && (
+                        <div onClick={addTask}>+ Add Events</div>
+                      )}
+                      {editingTask && editIndex === -1 && (
+                        <TextInput
+                          class="mt-0 rounded-md border-gray-300 bg-white disabled:border-gray-500 disabled:bg-gray-300"
+                          id="taskName"
+                          name="taskName"
+                          value={taskName}
+                          autoFocus={true}
+                          onChange={(e) => {
+                            setTaskName(e.target.value);
+                          }}
+                          onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                              saveTask(values, setFieldValue);
+                            }
+                          }}
+                          type="text"
+                          placeholder="Your task name"
+                        />
+                      )}
+                    </Dropdown.Header>
                   </Dropdown>
-                  {editingTask && (
+                  {/* {editingTask && (
                     <div>
                       <div className="mb-3 flex flex-col">
                         <div className="flex flex-row">
@@ -811,7 +903,7 @@ const EventFormModal = ({
                         />
                       </div>
                     </div>
-                  )}
+                  )} */}
                 </div>
               </Styled.Row>
             </Styled.ModalBody>
