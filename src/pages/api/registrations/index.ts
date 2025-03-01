1a799e0e0bd16e263aaa86590bdf88d50a2c2601
Johannes Qian <johannesq23@gmail.com>
2025-02-28 23:30:35 -0500

    remove console log

--- src/pages/api/registrations/index.ts
+++ src/pages/api/registrations/index.ts
@@ -43,7 +43,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     case "POST": {
       const result = registrationInputServerValidator.safeParse(req.body);
       if (!result.success) return res.status(400).json({ error: result.error });
-      console.log(result.data);
 
       await sendRegistrationConfirmationEmail(
         result.data.userId,
