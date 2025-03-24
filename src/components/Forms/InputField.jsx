d102465fd9c9c758452fa0cecef10a73a6ec9971
Bossixd <bosspattakit123@gmail.com>
2025-03-24 14:30:29 -0400

    red border time error fix

--- src/components/Forms/InputField.jsx
+++ src/components/Forms/InputField.jsx
@@ -37,8 +37,8 @@ const InputField = (props) => (
       {({ field }) => (
         <TextInput
           class={
-            "border-1 mt-0 h-10 w-full rounded-md border-gray-300 bg-white disabled:border-gray-500 disabled:bg-gray-300 " +
-            (props.invalid ? "border-2 border-[#a10101]" : "")
+            "border-1 mt-0 h-10 w-full rounded-md bg-white disabled:border-gray-500 disabled:bg-gray-300 " +
+            (props.invalid ? "border-2 border-[#a10101]" : "border-gray-300")
           }
           id={props.name}
           name={props.name}
