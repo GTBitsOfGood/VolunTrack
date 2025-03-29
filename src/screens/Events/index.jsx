dea14f441c355b72c4a9a4fd1a86d818b97445df
emilyliublair <emilyliu.us@gmail.com>
2025-03-29 14:44:11 -0400

    prevent weird long scroll

--- src/screens/Events/index.jsx
+++ src/screens/Events/index.jsx
@@ -1,5 +1,23 @@
+import { useEffect } from 'react';
 import EventManager from "./EventManager";
 
-const EventManagerSelector = () => <EventManager isHomePage={false} />;
+const EventManagerSelector = () => {
+  useEffect(() => {
+    const nextRoot = document.getElementById('__next');
+    if (nextRoot) {
+      nextRoot.style.overflowY = 'hidden'; // Disable scroll
+    }
+
+    return () => {
+      if (nextRoot) {
+        nextRoot.style.overflowY = 'auto'; // Re-enable on unmount
+      }
+    };
+  }, []);
+
+  return (
+    <EventManager isHomePage={false} />
+  );
+};
 
 export default EventManagerSelector;
