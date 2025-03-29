cf8ecdec975bf089f2bc340551631277431b598f
ylw-PotatoTornado <yvwu510@gmail.com>
2025-03-29 13:35:02 -0400

    [UPDATE] src/queries/attendances error handling for attendances queries

--- src/queries/attendances.ts
+++ src/queries/attendances.ts
@@ -8,23 +8,34 @@ import {
 import { QueryPartialMatch } from "./index";
 
 // Helper functions for checking in and out volunteers
-export const checkInVolunteer = (
+export const checkInVolunteer = async (
   userId: Types.ObjectId,
   eventId: Types.ObjectId,
   organizationId: Types.ObjectId,
   volunteerName: string,
   volunteerEmail: string,
   eventName: string
-) =>
-  createAttendance({
-    userId,
-    eventId,
-    organizationId,
-    volunteerName,
-    volunteerEmail,
-    eventName,
-    checkinTime: new Date(),
-  });
+) => {
+  try {
+    const response = await createAttendance({
+      userId,
+      eventId,
+      organizationId,
+      volunteerName,
+      volunteerEmail,
+      eventName,
+      checkinTime: new Date(),
+    });
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        error: error.response?.data?.error || error.message,
+      };
+    }
+    return { error: "Error checking in volunteer." };
+  }
+};
 
 export const checkOutVolunteer = async (
   userId: Types.ObjectId,
@@ -40,74 +51,147 @@ export const checkOutVolunteer = async (
     null,
     null
   );
+
+  if ("error" in attendanceResponse) {
+    console.error("Error fetching attendances:", attendanceResponse.error);
+    return;
+  }
+
   const attendanceId = attendanceResponse.data.attendances?.[0]._id;
 
   if (!attendanceId) return;
   return updateAttendance(attendanceId, { checkoutTime: new Date() });
 };
 
-export const getAttendance = (attendanceId: Types.ObjectId) =>
-  axios.get<{
-    attendance?: AttendanceDocument;
-    error?: ZodError | string;
-  }>(`/api/attendances/${attendanceId.toString()}`);
+export const getAttendance = async (attendanceId: Types.ObjectId) => {
+  try {
+    const response = await axios.get<{
+      attendance?: AttendanceDocument;
+      error?: ZodError | string;
+    }>(`/api/attendances/${attendanceId.toString()}`);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        error: error.response?.data?.error || error.message,
+      };
+    }
+    return { error: "Error checking in volunteer." };
+  }
+};
 
-export const getAttendances = (
+export const getAttendances = async (
   query: QueryPartialMatch,
   checkinTimeStart?: Date,
   checkinTimeEnd?: Date,
   checkoutTimeStart?: Date | null,
   checkoutTimeEnd?: Date | null
-) =>
-  axios.get<{
-    attendances?: AttendanceDocument[];
-    error?: ZodError | string;
-  }>("/api/attendances", {
-    params: {
-      userId: query.userId,
-      eventId: query.eventId,
-      organizationId: query.organizationId,
-      checkinTimeStart,
-      checkinTimeEnd,
-      checkoutTimeStart,
-      checkoutTimeEnd,
-    },
-  });
+) => {
+  try {
+    const response = await axios.get<{
+      attendances?: AttendanceDocument[];
+      error?: ZodError | string;
+    }>("/api/attendances", {
+      params: {
+        userId: query.userId,
+        eventId: query.eventId,
+        organizationId: query.organizationId,
+        checkinTimeStart,
+        checkinTimeEnd,
+        checkoutTimeStart,
+        checkoutTimeEnd,
+      },
+    });
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        error: error.response?.data?.error || error.message,
+      };
+    }
+    return { error: "Error checking in volunteer." };
+  }
+};
 
-export const createAttendance = (attendanceInput: AttendanceInputClient) =>
-  axios.post<{
-    attendance?: AttendanceDocument;
-    error?: ZodError | string;
-  }>("/api/attendances", attendanceInput);
+export const createAttendance = async (
+  attendanceInput: AttendanceInputClient
+) => {
+  try {
+    const response = await axios.post<{
+      attendance?: AttendanceDocument;
+      error?: ZodError | string;
+    }>("/api/attendances", attendanceInput);
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        error: error.response?.data?.error || error.message,
+      };
+    }
+    return { error: "Error creating attendance." };
+  }
+};
 
-export const updateAttendance = (
+export const updateAttendance = async (
   attendanceId: Types.ObjectId,
   attendanceInput: Partial<AttendanceInputClient>
-) =>
-  axios.put<{
-    attendance?: AttendanceDocument;
-    error?: ZodError | string;
-  }>(`/api/attendances/${attendanceId.toString()}`, attendanceInput);
+) => {
+  try {
+    const response = await axios.put<{
+      attendance?: AttendanceDocument;
+      error?: ZodError | string;
+    }>(`/api/attendances/${attendanceId.toString()}`, attendanceInput);
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        error: error.response?.data?.error || error.message,
+      };
+    }
+    return { error: "Error creating attendance." };
+  }
+};
 
-export const deleteAttendance = (attendanceId: Types.ObjectId) =>
-  axios.delete<{ error?: ZodError | string }>(
-    `/api/attendances/${attendanceId.toString()}`
-  );
+export const deleteAttendance = async (attendanceId: Types.ObjectId) => {
+  try {
+    const response = await axios.delete<{ error?: ZodError | string }>(
+      `/api/attendances/${attendanceId.toString()}`
+    );
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        error: error.response?.data?.error || error.message,
+      };
+    }
+    return { error: "Error creating attendance." };
+  }
+};
 
-export const getAttendanceStatistics = (
+export const getAttendanceStatistics = async (
   organizationId?: Types.ObjectId,
   startDate?: Date,
   endDate?: Date
-) =>
-  axios.get<
-    {
-      statistics?: {
-        _id: string;
-        num: number;
-        users: Types.ObjectId[];
-        minutes: number;
+) => {
+  try {
+    const response = await axios.get<
+      {
+        statistics?: {
+          _id: string;
+          num: number;
+          users: Types.ObjectId[];
+          minutes: number;
+        };
+      }[]
+    >("/api/attendances/statistics", {
+      params: { organizationId, startDate, endDate },
+    });
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return {
+        error: error.response?.data?.error || error.message,
       };
-    }[]
-  >("/api/attendances/statistics", {
-    params: { organizationId, startDate, endDate },
-  });
+    }
+    return { error: "Error creating attendance." };
+  }
+};
