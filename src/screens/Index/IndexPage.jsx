865773ef289b34c47459c9f36dcb910bfafecf73
Ivan Wang <ivanwang1239@gmail.com>
2026-02-22 14:20:19 -0500

    Adding loading screen for indexPage

--- src/screens/Index/IndexPage.jsx
+++ src/screens/Index/IndexPage.jsx
@@ -2,6 +2,7 @@ import { useSession } from "next-auth/react";
 import router from "next/router";
 import "normalize.css";
 import { useEffect } from "react";
+import LoadingScreen from "../../components/LoadingScreen";
 
 const IndexPage = () => {
   const {
@@ -12,10 +13,7 @@ const IndexPage = () => {
     router.push("/home");
   });
 
-  return (
-    <>
-    </>
-  );
+  return <LoadingScreen />;
 };
 
 export default IndexPage;
