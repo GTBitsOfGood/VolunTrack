6c8d63dfa2e4bdf9fa32aa205faf2d7e3fa6e1e7
Spencer Banko <151697851+spencer-banko@users.noreply.github.com>
2026-02-21 21:31:33 -0500

    Inconsistent padding when editing event (#552)
    * Fixed font to be consistent with rest of application, fixed typo in Multiple Choice, fixed the logic for the toggling question, and also fixed a typo with the checkboxes that was causing them not to appear
    
    * fixed spacing and padding issues
    
    * Refactor Edit Event modal header: left-align text, extend border line, and reposition close button
    
    * fixed linter issues
    
    * added back the optional chaining
    
    ---------
    
    Co-authored-by: Spencer Banko <spencer@lockchain.io>
    Co-authored-by: Ivan Wang <114537147+ivanwangg@users.noreply.github.com>

--- src/screens/Events/Admin/EventEditModal.jsx
+++ src/screens/Events/Admin/EventEditModal.jsx
@@ -19,6 +19,10 @@ const Styled = {
       padding-left: 3.5rem;
       border-bottom: 2px solid ${variables["dark"]};
     }
+    button.close {
+      margin-top: 0.2rem;
+      margin-right: 0.5rem;
+    }
   `,
 };
 
@@ -97,10 +101,10 @@ const EventEditModal = ({
         </div>
       )}
       {recurringEventConfirm && (
-        <div>
+        <div className="pl-4">
           <Styled.ModalHeader toggle={toggle} />
           <Text
-            className="mx-auto w-3/4 border-b-2 border-b-primaryColor text-center text-primaryColor"
+            className="-mt-12 w-[calc(100%-2.5rem)] border-b-2 border-b-primaryColor pb-3 text-left text-primaryColor"
             text="Edit Event"
             type="subheader"
           />
